834055101ece4fd5b10fdb2da9407a66be405784
Creed Naylor <cnaylor@steveregan.loc>
2018-09-24 15:07:36 -0600

    add files

---
+++ bank rec - 2.py
@@ -0,0 +1,49 @@
+
+print("Cmt: Importing modules...")
+
+import time, win32com.client, os
+
+start_time = time.time()
+print("Cmt: Importing modules...Done.")
+print("Cmt: Open and connect to file...")
+
+def emptyStr(s):
+    if s:
+        return str(s)
+    else:
+        return ""
+
+excelApp = win32com.client.gencache.EnsureDispatch('Excel.Application')
+filePath = os.path.abspath(os.curdir)
+print(filePath)
+fileName = "Bank_Rec.xlsx"
+
+excelApp.Workbooks.Open(filePath + "\\" + fileName)
+excelWb = excelApp.Workbooks(fileName)
+
+excelBankTableSheet = excelWb.Worksheets("Bank Table")
+excelBankTableSearchSheet = excelWb.Worksheets("Bank Table Search")
+excelApp.Visible = True
+
+print("Cmt: Open and connect to file...Done.")
+
+
+row = 1
+
+while excelBankTableSheet.Cells(row, 1).Value:
+    for bankCol in range(1, 7):
+        excelBankTableSearchSheet.Cells(row, bankCol).Value = excelBankTableSheet.Cells(row, bankCol).Value
+
+    excelBankTableSearchSheet.Cells(row, 7).Value = excelBankTableSheet.Cells(row, 9).Value
+
+    row = row + 1
+
+
+excelBankTableSearchSheet.Cells.EntireColumn.AutoFit()
+excelWb.Save()
+
+print("Elapsed time is " + str(time.time() - start_time))
+
+
+
+
