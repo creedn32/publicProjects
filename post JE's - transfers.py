617b3126134380bb1ea34b3bc1f9bca67335e3b5
Creed Naylor <cnaylor@steveregan.loc>
2018-10-03 16:25:03 -0600

    update to VBA-based code

--- post JE's - transfers.py
+++ post JE's - transfers.py
@@ -1,17 +1,28 @@
 print("Cmt: Importing modules...")
 
-import sys, time, xlwings, pywinauto, pyautogui, win32api    
+import os, pywinauto, pyautogui, win32com.client, time, win32api # sys   
 
 print("Cmt: Importing modules...Done.")
 
-
 print("Cmt: Open and connect to file...")
 
-pyautogui.PAUSE = .0
-row = 583
-filePath = ""
-jeWb = xlwings.Book(filePath)
-sheetBankTransfers = jeWb.sheets["Bank Transfers"]
+excelApp = win32com.client.gencache.EnsureDispatch('Excel.Application')
+excelApp.DisplayAlerts = False
+filePath = os.path.abspath(os.curdir)
+fileName = "JE's To Post"
+fileExtension = ".xlsx"
+
+excelApp.Workbooks.Open(filePath + "\\" + fileName + fileExtension)
+excelApp.Calculation = win32com.client.constants.xlCalculationManual
+
+excelWb = excelApp.Workbooks(fileName + fileExtension)
+
+excelBankTransfersSheet = excelWb.Worksheets("Bank Transfers")
+
+pyautogui.PAUSE = 0
+bankTransfersRow = 3
+bankTransfersRow = 729
+
 
 for win in pywinauto.findwindows.find_elements():
     gpWinTitleShort = "Bank Transfer Entry"
@@ -20,45 +31,61 @@ for win in pywinauto.findwindows.find_elements():
 
 pywinauto.win32functions.SetForegroundWindow(pywinauto.findwindows.find_window(title=gpWinTitleFull))
 
+excelApp.Visible = True
 print("Cmt: Open and connect to file...Done.")
 
 
-while not (sheetBankTransfers.range(row, 1).value == None):
 
-    if sheetBankTransfers.range(row, 1).color == None:
+while excelBankTransfersSheet.Cells(bankTransfersRow, 1).Value:
 
-        print("Row " + str(row) + " will be entered.")
+##    print(excelBankTransfersSheet.Cells(bankTransfersRow, 1).Interior.Color)
 
+    if excelBankTransfersSheet.Cells(bankTransfersRow, 1).Interior.Color == 16777215:
+    
+        print("Row " + str(bankTransfersRow) + " will be entered.")
+        
         pyautogui.press(["tab", "tab"])
 
         for x in range(1, 6):
-            print("x is " + str(x))
-            print(sheetBankTransfers.range(row, x).value)
+            #print("x is " + str(x))
+            print(excelBankTransfersSheet.Cells(bankTransfersRow, x).Value)
+
+
+            if x == 1:
+                #string = "090718"
+                #string = ('%02d' % excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.month) + ('%02d' % excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.day) + str(excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.year)
+                string = ('%02d' % excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.month) + ('%02d' % excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.day) + str(excelBankTransfersSheet.Cells(bankTransfersRow, x).Value.year)
+            else:
+                string = str(excelBankTransfersSheet.Cells(bankTransfersRow, x).Value)
+
 
             if x == 4:
                 pyautogui.press("tab")
-
-            if x == 1:
-                string = ('%02d' % sheetBankTransfers.range(row, x).value.month) + ('%02d' % sheetBankTransfers.range(row, x).value.day) + str(sheetBankTransfers.range(row, x).value.year)
-            else:
-                string= str(sheetBankTransfers.range(row, x).value)
-
+                
             for letter in string:
                 pyautogui.press(letter)
 
+            #if x == 3:
+                #pyautogui.press("tab")
             if x == 5:
-                pyautogui.press("tab")
+                 pyautogui.press("tab")
+                 pyautogui.press("tab")
                 
             pyautogui.press("tab")
 
 
-
         while True:
-            if win32api.GetKeyState(0x01) == -127:
+            if win32api.GetKeyState(0x01) == -127 or win32api.GetKeyState(0x01) == -128:
                 print("left button clicked")
-                time.sleep(1)
+                time.sleep(1.5)
                 break
 
-    row = row + 1
+    
+    bankTransfersRow = bankTransfersRow + 1
 
+
+
+excelApp.DisplayAlerts = True
+excelApp.Calculation = win32com.client.constants.xlCalculationAutomatic
+excelWb.Save()
 
