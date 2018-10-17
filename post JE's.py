c8b0f5429a0284a88c39a1f642cfdc4c614f5c5e
Creed Naylor <cnaylor@steveregan.loc>
2018-10-17 15:20:59 -0600

    add Decrease Adjustment feature

--- post JE's.py
+++ post JE's.py
@@ -9,31 +9,24 @@ import time, xlwings, pywinauto, pyautogui, win32api, os, win32com.client
 #print("execfile(\"" + inspect.getfile(inspect.currentframe()).encode('string-escape') + "\")")
 
 print("Cmt: Importing modules...Done.")
+print("Cmt: Open and connect to file...")
 
 excelApp = win32com.client.gencache.EnsureDispatch('Excel.Application')
+excelApp.Visible = True
 excelApp.DisplayAlerts = False
+
 filePath = os.path.abspath(os.curdir)
 fileName = "JE's To Post"
 fileExtension = ".xlsx"
-
 excelApp.Workbooks.Open(filePath + "\\" + fileName + fileExtension)
-excelApp.Calculation = win32com.client.constants.xlCalculationManual
 
+excelApp.Calculation = win32com.client.constants.xlCalculationManual
 excelWb = excelApp.Workbooks(fileName + fileExtension)
 excelBankTransactionsSheet = excelWb.Worksheets("Bank Transactions")
-
 pyautogui.PAUSE = 0
 # pyautogui.FAILSAFE = True
-
-excelApp.Visible = True
-
-print("Cmt: Open and connect to file...")
-
 bankTransactionsRow = 2
-bankTransactionsRow = 982
-
-
-
+bankTransactionsRow = 961
 
 for win in pywinauto.findwindows.find_elements():
     gpWinTitleShort = "Bank Transaction Entry"
@@ -42,10 +35,8 @@ for win in pywinauto.findwindows.find_elements():
 
 pywinauto.win32functions.SetForegroundWindow(pywinauto.findwindows.find_window(title=gpWinTitleFull))
 
+
 print("Cmt: Open and connect to file...Done.")
-
-
-
 
 
 while excelBankTransactionsSheet.Cells(bankTransactionsRow, 1).Value:
@@ -53,14 +44,17 @@ while excelBankTransactionsSheet.Cells(bankTransactionsRow, 1).Value:
     if excelBankTransactionsSheet.Cells(bankTransactionsRow, 1).Interior.Color == 16777215 and excelBankTransactionsSheet.Cells(bankTransactionsRow, 8).Value:
     
         print("Row " + str(bankTransactionsRow) + " will be entered.")
+        debitFirst = True
         
 
         for x in range(1, 10):
-            print(excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value)
+
+            string = str(excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value)
+
             if x == 3:
                 string = ('%02d' % excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value.month) + ('%02d' % excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value.day) + str(excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value.year)
-            else:
-                string = str(excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value)
+            elif str(excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value) == "None":
+                string = ""
 
             
             if x == 1:
@@ -70,90 +64,40 @@ while excelBankTransactionsSheet.Cells(bankTransactionsRow, 1).Value:
                     pyautogui.press(["down", "up", "up", "up", "down"])
             elif x == 2:
                 if excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value == "Check":
-                    pyautogui.press(["down", "up", "up"])
+                    pyautogui.press(["down", "up", "up", "up"])
                 if excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value == "Cash":
-                    pyautogui.press(["down", "up", "up", "down"])              
+                    pyautogui.press(["down", "up", "up", "up", "down"])
+                if excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value == "Increase Adjustment":
+                    pyautogui.press(["down", "up", "up", "up", "down", "down"])
+                if excelBankTransactionsSheet.Cells(bankTransactionsRow, x).Value == "Decrease Adjustment":
+                    debitFirst = False
+                    pyautogui.press(["down", "up", "up", "up", "down", "down", "down"])
             else:
                 for letter in string:
                     pyautogui.press(letter)
 
 
+            extraTabs = 1
+            
+            if x == 4:
+                extraTabs = 2 #extra tabs after entering Checkbook ID
+            elif x == 7:
+                extraTabs = 6 #extra tabs after entering amount
+            elif x == 8 and debitFirst:
+                extraTabs = 2 #extra tabs after entering GL Account       
 
-            if x == 4 or x == 8:
+            for i in range(1, extraTabs + 1):
                 pyautogui.press("tab")
-                
-            if x == 7:
-                for x in range(1, 5):
-                    pyautogui.press("tab")
 
-                for letter in string:
-                    pyautogui.press(letter)
-                pyautogui.press("tab")
-
-            pyautogui.press("tab")
-
-
-
+ 
         while True:
             if win32api.GetKeyState(0x01) == -127 or win32api.GetKeyState(0x01) == -128:
                 print("left button clicked")
-                time.sleep(1.5)
+                time.sleep(4)
                 break
 
     
     bankTransactionsRow = bankTransactionsRow + 1
-
-
-
-
-
-
-
-##
-##        for currentColumn in range(1, 10):
-##        
-##            if currentColumn == 5:
-##                pyautogui.press("tab")
-##
-##
-##            if excelSheet.range(row, 2).value == "Increase Adjustment" or excelSheet.range(row, 2).value == "Check":
-##                if currentColumn == 8:
-##                    for tabCount in range(0, 5):
-##                        pyautogui.press("tab")
-##
-##                if currentColumn == 9:
-##                    for tabCount in range(0, 1):
-##                        pyautogui.press("tab")
-##                                                   
-##            elif excelSheet.range(row, 2).value == "Decrease Adjustment":
-##                if currentColumn == 8:
-##                    for tabCount in range(0, 5):
-##                        pyautogui.press("tab")
-##
-##                        
-##
-##            for letter in str(excelSheet.range(row, currentColumn).value):
-##                if currentColumn == 1:
-##                    pyautogui.press(["down", "up", "up", "up"])
-##                    
-##                    if excelSheet.range(row, 1).value == "Enter Transaction":
-##                        break
-##                    elif excelSheet.range(row, 1).value == "Enter Receipt":
-##                        pyautogui.press(letter)
-##                        break
-##
-##                if currentColumn == 2 and excelSheet.range(row, 1).value == "Enter Transaction":
-##                    pyautogui.press(letter)
-##                    break
-##                elif currentColumn == 2 and excelSheet.range(row, 1).value == "Enter Receipt":
-##                    break
-##
-##                if currentColumn == 3 and letter == ".":
-##                    break
-##                
-##                pyautogui.press(letter)
-
-
 
 
 excelApp.DisplayAlerts = True
