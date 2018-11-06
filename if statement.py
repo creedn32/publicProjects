e7c3ab373df46884864bfa852ee270368f7fe376
Creed Naylor <cnaylor@steveregan.loc>
2018-11-06 16:35:56 -0700

    added old code

---
+++ if statement.py
@@ -0,0 +1,20 @@
+programName = 'G'
+
+if programName == 'Bistrack':
+    print('Running Bistrack')
+elif programName == 'GP':
+    print('Running GP')
+else:
+    print('Not running anything.')
+
+
+spam = 0
+while spam < 5:
+    print('asdf')
+    spam = spam + 1
+
+
+print ('Name is')
+for i in range(5):
+    print('Jimmy Five Times(' + str(i) + ')')
+    
