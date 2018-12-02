37689a2e4316c642b9fd8dddc6fdbe4710efc492
ctn <creednaylor@gmail.com>
2018-12-02 12:00:59 -0700

    download table data

---
+++ download table data.py
@@ -0,0 +1,30 @@
+print("Importing modules, setting up variables, and setting up objects...")
+
+import win32com.client, time, datetime, bs4, os
+from selenium import webdriver
+
+
+
+pageLoadTime = 15
+chromeDriverCapabilities = webdriver.common.desired_capabilities.DesiredCapabilities.CHROME
+chromeDriverCapabilities["pageLoadStrategy"] = "none"
+chromeDriver = webdriver.Chrome(desired_capabilities=chromeDriverCapabilities)
+chromeDriver.set_window_position(-1000, 0)
+chromeDriver.maximize_window()
+
+limits = [50, 500, 1000]
+
+chromeDriver.get("https://www..com/")
+time.sleep(pageLoadTime)
+
+chromeDriver.find_element_by_xpath("//*[@id=\"blogin\"]/li[2]/a").click()
+time.sleep(pageLoadTime / 3)
+
+chromeDriver.find_element_by_xpath("//*[@id=\"Email\"]").send_keys("example@gmail.com")
+chromeDriver.find_element_by_xpath("//*[@id=\"Password\"]").send_keys("password")
+chromeDriver.find_element_by_xpath("//*[@id=\"login\"]").click()
+time.sleep(pageLoadTime)
+
+
+
+
