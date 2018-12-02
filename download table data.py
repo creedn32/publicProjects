01b9acb634a28ad5a26ba1577f3abd55fd017b2f
ctn <creednaylor@gmail.com>
2018-12-02 12:08:22 -0700

    download table data using config.py

--- download table data.py
+++ download table data.py
@@ -1,6 +1,6 @@
 print("Importing modules, setting up variables, and setting up objects...")
 
-import win32com.client, time, datetime, bs4, os
+import win32com.client, time, datetime, bs4, os, config
 from selenium import webdriver
 
 
@@ -14,14 +14,14 @@ chromeDriver.maximize_window()
 
 limits = [50, 500, 1000]
 
-chromeDriver.get("https://www..com/")
+chromeDriver.get(config.website)
 time.sleep(pageLoadTime)
 
 chromeDriver.find_element_by_xpath("//*[@id=\"blogin\"]/li[2]/a").click()
 time.sleep(pageLoadTime / 3)
 
-chromeDriver.find_element_by_xpath("//*[@id=\"Email\"]").send_keys("example@gmail.com")
-chromeDriver.find_element_by_xpath("//*[@id=\"Password\"]").send_keys("password")
+chromeDriver.find_element_by_xpath("//*[@id=\"Email\"]").send_keys(config.username)
+chromeDriver.find_element_by_xpath("//*[@id=\"Password\"]").send_keys(config.password)
 chromeDriver.find_element_by_xpath("//*[@id=\"login\"]").click()
 time.sleep(pageLoadTime)
 
