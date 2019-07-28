d734e4832c614ddfcf8fe7853855eef285645313
ctn <creednaylor@gmail.com>
2019-07-28 11:51:56 -0600

    add dragging

--- movemouse.py
+++ movemouse.py
@@ -1,8 +1,17 @@
 import pyautogui
-for i in range(1):
-    pyautogui.moveTo(100, 100, duration=0.25)
-    pyautogui.moveTo(200, 100, duration=0.25)
-    pyautogui.moveTo(200, 200, duration=0.25)
-    pyautogui.moveTo(100, 200, duration=0.25)
+pyautogui.PAUSE = 1
+
+
+# for i in range(1):
+#    pyautogui.moveTo(100, 100, duration=0.25)
+#    pyautogui.moveTo(200, 100, duration=0.25)
+#    pyautogui.moveTo(200, 200, duration=0.25)
+#    pyautogui.moveTo(100, 200, duration=0.25)
 
 print(pyautogui.position())
+
+
+for i in range(2):
+    pyautogui.click(648, 347)
+    pyautogui.dragRel(90, 0, duration=1)
+
