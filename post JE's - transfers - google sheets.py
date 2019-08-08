72e2c0f1d4d1784d4c2d81e599ed959a9ab8d5cc
creedn32 <creednaylor@gmail.com>
2019-08-08 14:30:06 -0600

    lastest updates

--- post JE's - transfers - google sheets.py
+++ post JE's - transfers - google sheets.py
@@ -1,13 +1,14 @@
-#check if modules are already imported
-
-
-
 print("Comment: Importing modules and setting up variables...")
+
+#Things to do:
+#
+##Check if modules are already imported
+
 
 import sys
 sys.path.append('..')
 
-import gspread, pyautogui, datetime, win32api, time, creed_modules.creed_toolpack
+import gspread, pyautogui, datetime, win32api, time, creed_modules.creed_toolpack, pynput.mouse
 from oauth2client.service_account import ServiceAccountCredentials
 
 
@@ -20,16 +21,26 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(credentialsPath, 
 googleSheetApp = gspread.authorize(credentials)
 googleSheetJournalEntries = googleSheetApp.open('Journal Entries To Post').worksheet('Bank Transfers')
 
+
+def functionOnClick(x, y, button, pressed):
+    print("Mouse clicked at {0}, {1} with {2} and pressed is {3}".format(x, y, button, pressed))
+    listenerObj.stop()
+
+
 print("Comment: Importing modules and setting up variables...Done.")
 
-time.sleep(2)
+
+with pynput.mouse.Listener(on_click=functionOnClick) as listenerObj:
+    print("Click on 'Clear' to begin posting...")
+    listenerObj.join()
+
 
 
 row = 24
 
 while googleSheetJournalEntries.cell(row, 1).value:
 
-    print("Row " + str(row) + " will be entered.")
+    print("Row " + str(row) + " will be populated into the Great Plains entry window.")
 
     creed_modules.creed_toolpack.repetitiveKeyPress(2, 'tab')
 
@@ -71,87 +82,12 @@ while googleSheetJournalEntries.cell(row, 1).value:
 
         creed_modules.creed_toolpack.repetitiveKeyPress(numberTabs, 'tab')
 
-    print("Waiting for left button click...")
 
-    while True:
-
-        keyPressed = win32api.GetKeyState(0x01)
-
-        if keyPressed in [-127, -128]:
-            print("left button clicked: " + str(keyPressed))
-            break
+    with pynput.mouse.Listener(on_click=functionOnClick) as listenerObj:
+        print("'Post' or 'Clear' this entry to continue...")
+        listenerObj.join()
 
 
     row = row + 1
 
 
-
-# from apiclient import discovery
-# from httplib2 import Http
-# from oauth2client import file, client, tools
-#
-# SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-# store = file.Storage('storage.json')
-# creds = store.get()
-# if not creds or creds.invalid:
-#     flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-#     creds = tools.run_flow(flow, store)
-# SHEETS = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
-#
-# SHEET_ID = ... # add your Sheet ID here
-# reqs = {'requests': [
-#     # frozen row 1
-#     {'updateSheetProperties': {
-#         'properties': {'gridProperties': {'frozenRowCount': 1}},
-#         'fields': 'gridProperties.frozenRowCount',
-#     }},
-#     # embolden row 1
-#     {'repeatCell': {
-#         'range': {'endRowIndex': 1},
-#         'cell': {'userEnteredFormat': {'textFormat': {'bold': True}}},
-#         'fields': 'userEnteredFormat.textFormat.bold',
-#     }},
-#     # currency format for column E (E2:E7)
-#     {'repeatCell': {
-#         'range': {
-#             'startRowIndex': 1,
-#             'endRowIndex': 6,
-#             'startColumnIndex': 4,
-#             'endColumnIndex': 5,
-#         },
-#         'cell': {
-#             'userEnteredFormat': {
-#                 'numberFormat': {
-#                     'type': 'CURRENCY',
-#                     'pattern': '"$"#,##0.00',
-#                 },
-#             },
-#         },
-#         'fields': 'userEnteredFormat.numberFormat',
-#     }},
-#     # validation for column F (F2:F7)
-#     {'setDataValidation': {
-#         'range': {
-#             'startRowIndex': 1,
-#             'endRowIndex': 6,
-#             'startColumnIndex': 5,
-#             'endColumnIndex': 6,
-#         },
-#         'rule': {
-#             'condition': {
-#                 'type': 'ONE_OF_LIST',
-#                 'values': [
-#                     {'userEnteredValue': 'PENDING'},
-#                     {'userEnteredValue': 'SHIPPED'},
-#                     {'userEnteredValue': 'DELIVERED'},
-#                 ]
-#             },
-#             #'inputMessage': 'Select PENDING, SHIPPED, or DELIVERED',
-#             #'strict': True,
-#             'showCustomUi': True,
-#         },
-#     }},
-# ]}
-#
-# res = SHEETS.spreadsheets().batchUpdate(
-#         spreadsheetId=SHEET_ID, body=reqs).execute()
---
+++ useGoogleSheetsAPI.py
@@ -0,0 +1,70 @@
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+SHEETS = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
+
+SHEET_ID = ... # add your Sheet ID here
+reqs = {'requests': [
+    # frozen row 1
+    {'updateSheetProperties': {
+        'properties': {'gridProperties': {'frozenRowCount': 1}},
+        'fields': 'gridProperties.frozenRowCount',
+    }},
+    # embolden row 1
+    {'repeatCell': {
+        'range': {'endRowIndex': 1},
+        'cell': {'userEnteredFormat': {'textFormat': {'bold': True}}},
+        'fields': 'userEnteredFormat.textFormat.bold',
+    }},
+    # currency format for column E (E2:E7)
+    {'repeatCell': {
+        'range': {
+            'startRowIndex': 1,
+            'endRowIndex': 6,
+            'startColumnIndex': 4,
+            'endColumnIndex': 5,
+        },
+        'cell': {
+            'userEnteredFormat': {
+                'numberFormat': {
+                    'type': 'CURRENCY',
+                    'pattern': '"$"#,##0.00',
+                },
+            },
+        },
+        'fields': 'userEnteredFormat.numberFormat',
+    }},
+    # validation for column F (F2:F7)
+    {'setDataValidation': {
+        'range': {
+            'startRowIndex': 1,
+            'endRowIndex': 6,
+            'startColumnIndex': 5,
+            'endColumnIndex': 6,
+        },
+        'rule': {
+            'condition': {
+                'type': 'ONE_OF_LIST',
+                'values': [
+                    {'userEnteredValue': 'PENDING'},
+                    {'userEnteredValue': 'SHIPPED'},
+                    {'userEnteredValue': 'DELIVERED'},
+                ]
+            },
+            #'inputMessage': 'Select PENDING, SHIPPED, or DELIVERED',
+            #'strict': True,
+            'showCustomUi': True,
+        },
+    }},
+]}
+
+res = SHEETS.spreadsheets().batchUpdate(
+        spreadsheetId=SHEET_ID, body=reqs).execute()
