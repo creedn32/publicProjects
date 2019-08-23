40c2e53a138accd7c0e01d7d073e9690fb59d0c1
ctn <16467643+creedn32@users.noreply.github.com>
2019-08-23 10:48:05 -0600

    latest updates

--- pyWinHookTrial.py
+++ pyWinHookTrial.py
@@ -2,16 +2,40 @@ import pyWinhook
 import pythoncom
 
 
-def OnKeyboardEvent(event):
-
-  print(pyWinhook.GetKeyState(0x20))
-  
-  if pyWinhook.GetKeyState(0x41) > 0 and pyWinhook.HookConstants.IDToName(event.KeyID) == "S":
+def functionComboDetected():
     print("Combo detected")
 
 
-  return True
+def OnKeyboardEvent(event):
 
+  if event.MessageName == "key down":
+    if event.Key in comboList:
+      currentPressedKeys.append(event.Key)
+
+    if list(dict.fromkeys(currentPressedKeys)) == comboList and keyDownInfoObj.comboReleased:
+        keyDownInfoObj.comboReleased = False
+        functionComboDetected()
+
+    if comboList[0] in currentPressedKeys:
+      keyDownInfoObj.turnOffKeys = True
+
+
+  if event.MessageName == "key up":
+    if event.Key in comboList:
+      currentPressedKeys[:] = [x for x in currentPressedKeys if x != event.Key]
+
+    if len(currentPressedKeys) == 0:
+      keyDownInfoObj.comboReleased = True
+
+  if pyWinhook.GetKeyState(pyWinhook.HookConstants.VKeyToID('VK_CONTROL')) > 0 and pyWinhook.HookConstants.IDToName(event.KeyID) == "S":
+    print("Combo detected")
+
+  if comboList[0] not in currentPressedKeys:
+    keyDownInfoObj.turnOffKeys = False
+
+
+
+  print(currentPressedKeys)
 
 
 
@@ -20,25 +44,46 @@ def OnKeyboardEvent(event):
   # print('Time: %s' % event.Time)
   # print('Window: %s' % event.Window)
   # print('WindowName: %s' % event.WindowName)
-  # print('Ascii: %s' %  event.Ascii, chr(event.Ascii))
-  # print('Key: %s' %  event.Key)
-  # print('KeyID: %s' %  event.KeyID)
-  # print('ScanCode: %s' %  event.ScanCode)
-  # print('Extended: %s' %  event.Extended)
-  # print('Injected: %s' %  event.Injected)
-  # print('Alt %s' %  event.Alt)
-  # print('Transition %s' %  event.Transition)
+  # print('Ascii: %s' % event.Ascii, chr(event.Ascii))
+  # print('Key: %s' % event.Key)
+  # print('KeyID: %s' % event.KeyID)
+  # print('ScanCode: %s' % event.ScanCode)
+  # print('Extended: %s' % event.Extended)
+  # print('Injected: %s' % event.Injected)
+  # print('Alt %s' % event.Alt)
+  # print('Transition %s' % event.Transition)
   # print('---')
+
 
   # return True to pass the event to other handlers
   # return False to stop the event from propagating
 
+  if keyDownInfoObj.turnOffKeys:
+    return False
+  else:
+    return True
 
+
+
+
+class keyDownInfo():
+  def __init__(self, comboReleased, turnOffKeys):
+    self.comboReleased = comboReleased
+    self.turnOffKeys = turnOffKeys
+
+keyDownInfoObj = keyDownInfo(True, False)
+
+
+comboList = ["A", "S"]
+currentPressedKeys = []
 
 
 hookManagerObj = pyWinhook.HookManager()
 hookManagerObj.KeyDown = OnKeyboardEvent
+hookManagerObj.KeyUp = OnKeyboardEvent
 hookManagerObj.HookKeyboard()
 pythoncom.PumpMessages()
+
+
 
 
