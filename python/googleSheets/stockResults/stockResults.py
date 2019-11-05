ce378f4bcbe2eaeef09e172e69f6f7e7051fa1da
creednaylor <16467643+creednaylor@users.noreply.github.com>
2019-11-04 18:59:12 -0700

    latest updates

--- python/googleSheets/stockResults/stockResults.py
+++ python/googleSheets/stockResults/stockResults.py
@@ -17,15 +17,14 @@ class moduleNameClass:
     pass
 
 moduleName = "myGoogleSheetsPythonLibrary"
-myGoogleSheetsPythonLibrary = moduleNameClass()
-
-
+moduleNameObj = moduleNameClass()
 
 for filePath in pathlib.Path(pathlib.Path.cwd().parents[0]/moduleName).iterdir():
     if filePath.stem not in ["__init__", "__pycache__"]:
-        moduleObj = importlib.import_module(moduleName + "." + filePath.stem)
-        setattr(myGoogleSheetsPythonLibrary, filePath.stem, moduleObj)
+        importedModuleObj = importlib.import_module(moduleName + "." + filePath.stem)
+        setattr(moduleNameObj, filePath.stem, importedModuleObj)
 
+myGoogleSheetsPythonLibrary = moduleNameObj
 
 googleSheetsObj = myGoogleSheetsPythonLibrary.googleSheetsAuthenticate.authFunc()
 
