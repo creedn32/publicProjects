fbef396d6d86c5cbad7bbd99a61b2cc31268e816
creednaylor <16467643+creednaylor@users.noreply.github.com>
2019-11-14 16:54:49 -0700

    latest updates

--- python/googleSheets/myGoogleSheetsPythonLibrary/googleSheetsFunctions.py
+++ python/googleSheets/myGoogleSheetsPythonLibrary/googleSheetsFunctions.py
@@ -50,17 +50,17 @@ def getCellValue(dataObj, sheetPos, rowPos, colPos):
 
 
 
-
-def getCellValueNumber(dataObj, sheetPos, rowPos, colPos):
-    sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
-    currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
-    dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
-    currentRowsData = myPythonFunctions.getFromDict(dataOnSheet, "rowData")
-    currentRowData = myPythonFunctions.getFromDict(myPythonFunctions.getFromList(currentRowsData, rowPos), "values")
-    try:
-        return myPythonFunctions.getFromList(currentRowData, colPos)["effectiveValue"]["numberValue"]
-    except:
-        return getCellValue(dataObj, sheetPos, rowPos, colPos)
+#
+# def getCellValueNumber(dataObj, sheetPos, rowPos, colPos):
+#     sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
+#     currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
+#     dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
+#     currentRowsData = myPythonFunctions.getFromDict(dataOnSheet, "rowData")
+#     currentRowData = myPythonFunctions.getFromDict(myPythonFunctions.getFromList(currentRowsData, rowPos), "values")
+#     try:
+#         return myPythonFunctions.getFromList(currentRowData, colPos)["effectiveValue"]["numberValue"]
+#     except:
+#         return getCellValue(dataObj, sheetPos, rowPos, colPos)
 
 
 
@@ -80,45 +80,52 @@ def getCellValueEffective(dataObj, sheetPos, rowPos, colPos):
 
 
 
-
-def createValuesData(numRows, numCols, dataObj):
-
-    listObj = []
-
-    for indexOfRow in range(0, numRows):
-        currentRowData = []
-
-        for indexOfColumn in range(0, numCols):
-            currentRowData.append(getCellValue(dataObj, 0, indexOfRow, indexOfColumn))
-
-        listObj.append(currentRowData)
-
-    return listObj
-
-
+#
+# def createValuesData(numRows, numCols, dataObj):
+#
+#     listObj = []
+#
+#     for indexOfRow in range(0, numRows):
+#         currentRowData = []
+#
+#         for indexOfColumn in range(0, numCols):
+#             currentRowData.append(getCellValue(dataObj, 0, indexOfRow, indexOfColumn))
+#
+#         listObj.append(currentRowData)
+#
+#     return listObj
 
 
 
-def extractValuesAndTypes(numRows, numCols, dataObj, sheetPos):
-
-    listToReturn = []
-
-    for indexOfRow in range(0, numRows):
-        currentRowData = []
-
-        for indexOfColumn in range(0, numCols):
-            dictionary = getCellValueEffective(dataObj, sheetPos, indexOfRow, indexOfColumn)
-
-            if not isinstance(dictionary, str):
-                dictKey = list(dictionary.keys())[0]
-                currentRowData.append({"value": dictionary[dictKey], "type": dictKey})
-            else:
-                currentRowData.append({"value": "", "type": ""})
-
-        listToReturn.append(currentRowData)
 
 
-    return listToReturn
+
+
+
+def countRows(dataObj, sheetPos):
+    sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
+
+    # saveFile(sheetsData, pathlib.Path(pathlib.Path.cwd().parents[3]/"privateData"/"stockResults"/"sheetsData.json"))
+    # for i in sheetsData:
+    #     pp(str(i)[:50])
+
+    currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
+    dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
+    return len(myPythonFunctions.getFromDict(dataOnSheet, "rowData"))
+
+
+
+
+
+def countColumns(dataObj, sheetPos):
+    sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
+    currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
+    dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
+    currentRowsData = myPythonFunctions.getFromDict(dataOnSheet, "rowData")
+    currentRowData = myPythonFunctions.getFromDict(myPythonFunctions.getFromList(currentRowsData, 0), "values")
+    return len(currentRowData)
+
+
 
 
 
@@ -147,27 +154,26 @@ def extractValues(numRows, numCols, dataObj, sheetPos):
 
 
 
+def extractValuesAndTypes(numRows, numCols, dataObj, sheetPos):
 
-def countRows(dataObj, sheetPos):
-    sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
+    listToReturn = []
 
-    # saveFile(sheetsData, pathlib.Path(pathlib.Path.cwd().parents[3]/"privateData"/"stockResults"/"sheetsData.json"))
-    # for i in sheetsData:
-    #     pp(str(i)[:50])
+    for indexOfRow in range(0, numRows):
+        currentRowData = []
 
-    currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
-    dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
-    return len(myPythonFunctions.getFromDict(dataOnSheet, "rowData"))
+        for indexOfColumn in range(0, numCols):
+            dictionary = getCellValueEffective(dataObj, sheetPos, indexOfRow, indexOfColumn)
+
+            if not isinstance(dictionary, str):
+                dictKey = list(dictionary.keys())[0]
+                currentRowData.append({"value": dictionary[dictKey], "type": dictKey})
+            else:
+                currentRowData.append({"value": "", "type": ""})
+
+        listToReturn.append(currentRowData)
 
 
+    return listToReturn
 
 
-
-def countColumns(dataObj, sheetPos):
-    sheetsData = myPythonFunctions.getFromDict(dataObj, "sheets")
-    currentSheetData = myPythonFunctions.getFromList(sheetsData, sheetPos)
-    dataOnSheet = myPythonFunctions.getFromList(myPythonFunctions.getFromDict(currentSheetData, "data"), 0)
-    currentRowsData = myPythonFunctions.getFromDict(dataOnSheet, "rowData")
-    currentRowData = myPythonFunctions.getFromDict(myPythonFunctions.getFromList(currentRowsData, 0), "values")
-    return len(currentRowData)
 
