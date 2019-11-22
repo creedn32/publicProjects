980fec850b955e19090fceb0a26f844b7a0053c4
creednaylor <16467643+creednaylor@users.noreply.github.com>
2019-11-21 17:06:13 -0700

    latest updates

--- python/googleSheets/stockResults/runSQL.py
+++ python/googleSheets/stockResults/runSQL.py
@@ -21,7 +21,7 @@ finishSetupTime = myPythonFunctions.time.time()
 print("Comment: Importing modules and setting up variables...Done. " + str(round(finishSetupTime - startTime, 3)) + " seconds")
 
 
-tblName = "tblTransactionsScrubbed"
+tblName = "tblTScrub"
 
 columnsObj = OrderedDict()
 columnsObj["tranDate"] = "date"
@@ -40,15 +40,18 @@ columnsObj["shares"] = "float"
 sqlObj = myPythonFunctions.createDatabase("stockResults.db", str(pathlib.Path.cwd().parents[3]/"privateData"/"stockResults"), tblName, columnsObj)
 myPythonFunctions.populateTable(tranScrubRowTotal, tranScrubColTotal, tblName, tranScrubDataList, sqlObj["sqlCursor"], [0])
 
-sqlCommand = "select distinct broker, stockName, lot from " + tblName + " where stockName not in ('All Stocks', 'Cash') and broker not in ('Robinhood - Related', 'Motif - Related');"
+sqlList = ["drop table if exists tblPurchase;", "create table tblPurchase as select broker, stockName, lot, tranDate, sum(amount) from tblTScrub where account = 'Cash' and tranType like '%Purchase%' and tranType not like '%Group Shares%' group by broker, stockName, lot, tranDate;"]
+myPythonFunctions.executeSQLStatements(sqlList, sqlObj["sqlCursor"])
 
-sqlObj["sqlCursor"].execute(sqlCommand)
-queryResult = sqlObj["sqlCursor"].fetchall()
+sqlList = ["drop table if exists tblSale;", "create table tblSale as select broker, stockName, lot, tranDate, sum(amount) from tblTScrub where account = 'Cash' and tranType like '%Sale%' and tranType not like '%Group Shares%' group by broker, stockName, lot, tranDate;"]
+myPythonFunctions.executeSQLStatements(sqlList, sqlObj["sqlCursor"])
+
+sqlList = ["drop table if exists tblDividends;", "create table tblDividends as select broker, stockName, lot, sum(amount) from tblTScrub where account = 'Cash' and tranType like '%Dividend' group by broker, stockName, lot;"]
+myPythonFunctions.executeSQLStatements(sqlList, sqlObj["sqlCursor"])
+
+
+googleSheetsFunctions.populateSheet(1, 100, "SQL Query Result", googleSheetsObj, spreadsheetID, myPythonFunctions.getQueryResult("select * from tblDividends", sqlObj["sqlCursor"]), True)
 myPythonFunctions.closeDatabase(sqlObj["sqlConnection"])
-
-
-googleSheetsFunctions.populateSheet(1, 1, "SQL Query Result", googleSheetsObj, spreadsheetID, queryResult, True)
-
 
 
 # if os.path.exists(dbPath):
