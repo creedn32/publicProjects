29ce77fb42c65d8a476aca36f8cea45d0f6fc149
creednaylor <16467643+creednaylor@users.noreply.github.com>
2020-01-17 12:36:02 -0700

    latest updates

--- python/guiAutomation/markGoogleKeepItemsAsDone/markGoogleKeepItemsAsDone.py
+++ python/guiAutomation/markGoogleKeepItemsAsDone/markGoogleKeepItemsAsDone.py
@@ -10,23 +10,37 @@ splitTime = myPyFunc.printElapsedTime(False, "A path was added that can now be i
 import pyautogui, pynput.mouse, time
 from pprint import pprint as pp
 
+
+
+def funcOnClick(x, y, button, pressed):
+    if not pressed:
+        mouseUpPositionObj.upXPosition = x
+        mouseUpPositionObj.upYPosition = y
+
+        print("Mouse {2} was {0} at {1}.".format("pressed" if pressed else "released", (x, y), button))
+        return False
+
+
 class mouseUpPositionClass:
     pass
 
 mouseUpPositionObj = mouseUpPositionClass
-mouseUpPositionObj.upXPosition = 0
-mouseUpPositionObj.upYPosition = 0
 
 
-# myPyFunc.printPythonInfo(mouseUpPositionObj, 50)
+splitTime = myPyFunc.printElapsedTime(False, "Finished importing modules and setting up functions, classes, and objects")
 
-with pynput.mouse.Listener(on_click=myPyFunc.functionOnClick) as listenerObj:
+
+
+with pynput.mouse.Listener(on_click=funcOnClick) as listenerObj:
     listenerObj.join()
 
 
 
+for clickCount in range(0, 50):
+    pyautogui.click(mouseUpPositionObj.upXPosition, mouseUpPositionObj.upYPosition)
+    # time.sleep(.0000001)
 
-# time.sleep(.5)
-# pyautogui.click(840, 40)
+
+
 
 splitTime = myPyFunc.printElapsedTime(False, "Code finished")
