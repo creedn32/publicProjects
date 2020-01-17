ff94099534295e90f7391fb5fb8bb3970cd8dacb
creednaylor <16467643+creednaylor@users.noreply.github.com>
2020-01-17 11:44:06 -0700

    latest updates

---
+++ python/guiAutomation/markGoogleKeepItemsAsDone/markGoogleKeepItemsAsDone.py
@@ -0,0 +1,9 @@
+import pyautogui, pynput.mouse
+
+print("Starting script...")
+
+# with pynput.mouse.Listener(on_click=myPyFunc.functionOnClick) as listenerObj:
+#     print("b")
+#     listenerObj.join()
+
+
