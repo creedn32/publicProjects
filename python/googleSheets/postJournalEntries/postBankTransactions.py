218875e713ac86e3006a6c0d3eb81cda6be9da68
creednaylor <16467643+creednaylor@users.noreply.github.com>
2020-02-13 13:19:49 -0700

    latest updates

--- python/googleSheets/postJournalEntries/postBankTransactions.py
+++ python/googleSheets/postJournalEntries/postBankTransactions.py
@@ -1,6 +1,12 @@
 import sys, pathlib
+from pprint import pprint as pp
+
+# pp(pathlib.Path.cwd().parents[1])
 sys.path.append(str(pathlib.Path.cwd().parents[1]))
+# pp(sys.path)
+
 from myPyLib import myPostBankTransactions
 
+import datetime, pynput.mouse #, win32api, win32con, pyautogui
 
-myPostBankTransactions.postTrans("Bank Transactions")
+# myPostBankTransactions.postTrans("Bank Transactions")
