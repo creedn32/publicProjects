ff0e91f4e704ad9e3e36c2e3b94a4e60aa586dab
creednaylor <16467643+creednaylor@users.noreply.github.com>
2020-03-02 18:09:46 -0700

    a

--- python/googleSheets/postJournalEntries/postBankTransactions.py
+++ python/googleSheets/postJournalEntries/postBankTransactions.py
@@ -1,9 +1,7 @@
 import sys, pathlib
 from pprint import pprint as pp
 
-# pp(pathlib.Path.cwd().parents[1])
-sys.path.append(str(pathlib.Path.cwd().parents[1]))
-# pp(sys.path)
+sys.path.append(str(pathlib.Path.cwd()/"publicProjects/python"))
 
 from myPyLib import myPostBankTransactions
 
