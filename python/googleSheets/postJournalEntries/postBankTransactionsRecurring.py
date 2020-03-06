0893a009d23dd58164de24605f80265974dccd69
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-03-06 14:59:06 -0700

    latest updates

--- python/googleSheets/postJournalEntries/postBankTransactionsRecurring.py
+++ python/googleSheets/postJournalEntries/postBankTransactionsRecurring.py
@@ -3,4 +3,10 @@ sys.path.append(str(pathlib.Path.cwd().parents[1]))
 from myPyLib import myPostBankTransactions
 
 
+sys.path.append(str(pathlib.Path.cwd().parents[1]))
+# sys.path.append(str(pathlib.Path.cwd()/"publicProjects/python"))
+# pp(sys.path)
+
+from myPyLib import myPostBankTransactions
+
 myPostBankTransactions.postTrans("Bank Transactions - Recurring")
