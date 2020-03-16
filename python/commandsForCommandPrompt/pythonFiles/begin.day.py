9e4bc54bab0b4d88e24ebf5c167deadfb760036e
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-03-15 23:45:34 -0600

    latest updates

--- python/commandsForCommandPrompt/pythonFiles/begin.day.py
+++ python/commandsForCommandPrompt/pythonFiles/begin.day.py
@@ -1,15 +1,17 @@
 from pprint import pprint as pp
 from pathlib import Path
-import json, subprocess
+import json, subprocess, sys
 
 
+pc = sys.argv[1]
 pathToCurrentFile = Path(__file__).resolve()
 currentFileName = pathToCurrentFile.stem
-pathToJSON = str(pathToCurrentFile).replace("publicProjects", "privateData").replace("pythonFiles\\" + pathToCurrentFile.name, currentFileName + ".json")
+pathToJSON = str(pathToCurrentFile).replace("publicProjects", "privateData").replace("pythonFiles\\" + pathToCurrentFile.name, currentFileName + "." + pc + ".json")
 
 
 with open(pathToJSON, "r") as filePathObj:
     fileObj = json.load(filePathObj)
 
 for process in fileObj:
+    # pass
     subprocess.call(process)
