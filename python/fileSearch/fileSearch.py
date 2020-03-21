f89a00d6bc1fbf742e3b5284b807798f57f1afc6
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-03-21 12:53:46 -0600

    latest updates

--- python/fileSearch/fileSearch.py
+++ python/fileSearch/fileSearch.py
@@ -1,2 +1,28 @@
-import pathlib
+from pathlib import Path
+from pprint import pprint as pp
+import os
+
+thisPythonFilePath = Path(__file__).resolve()
+pathToPublicProjectsPython = thisPythonFilePath.parents[1]
+
+def listOfSubFolders(folderPath):
+    subFolderArray = []
+ 
+    for node in folderPath.iterdir():
+        if not node.is_file():
+            subFolderArray.append(node)
+
+    return subFolderArray
+
+
+folderArray = [pathToPublicProjectsPython]
+
+while folderArray:
+    currentFolder = folderArray.pop(0)
+    folderArray.extend(listOfSubFolders(currentFolder))
+    pp(currentFolder)
+
+
+
+
 
