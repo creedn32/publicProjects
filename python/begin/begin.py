adf2d1b433511be0e4195787af5ea55b0b1260df
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-03-23 09:03:42 -0600

    latest updates

--- python/begin/begin.py
+++ python/begin/begin.py
@@ -1,25 +1,28 @@
 from pathlib import Path
 import sys
+from runpy import run_path as runPath
+
 
 thisPythonFilePath = Path(__file__).resolve()
 pathToPublicProjectsPython = thisPythonFilePath.parents[1]
+pathToRepos = pathToPublicProjectsPython.parents[1]
 # sys.path.append(str(pathToPublicProjectsPython))
 
 import json, subprocess, sys, psutil
 from pprint import pprint as pp
 
 
-# currentMachine = __import__(sys.argv[1] + ".py")
-currentMachine = sys.argv[1]
+pathToPythonDataFile = Path(pathToRepos, 'privateData', 'python', 'begin', sys.argv[1] + '.py')
+currentMachine = runPath(pathToPythonDataFile)
 thisPythonFileStem = thisPythonFilePath.stem
-jsonPath = str(thisPythonFilePath).replace('publicProjects', 'privateData').replace('.py', '.' + currentMachine + '.json') # + thisPythonFileStem + currentMachine + '.json')
+# jsonPath = str(thisPythonFilePath).replace('publicProjects', 'privateData').replace('.py', '.' + currentMachine + '.json') # + thisPythonFileStem + currentMachine + '.json')
 
 
-with open(jsonPath, 'r') as filePathObj:
-    fileObj = json.load(filePathObj)
+# with open(jsonPath, 'r') as filePathObj:
+#     fileObj = json.load(filePathObj)
 
 
-for process in fileObj['processesToStart']:
+for process in currentMachine.get('processesToStart'):
     if process != '':
         # subprocess.Popen(process)
         pass
@@ -35,13 +38,23 @@ for proc in psutil.process_iter ():
 
         # print (f'execution module: {proc.exe ()} ')
         
-        if len(proc.cmdline()) > 1:
-            pp(proc.cmdline()[0])
-            pp(proc.cmdline()[1])
+        cmdLineDetailCount = 0
+
+        for cmdLineDetail in proc.cmdline():
+            if cmdLineDetailCount < 2:
+                pass
+                # print(str(cmdLineDetailCount) + ': ' + cmdLineDetail)
+            cmdLineDetailCount += 1
+
+
+        # if len(proc.cmdline()) > 1:
+            # pp(proc.cmdline()[0])
+            # pp(proc.cmdline()[1])
         
         # print (f'current directory: {proc.cwd ()} ')
     except psutil.AccessDenied:
-        print ('(You do not have access to this process)')
+        pass
+        # print ('(You do not have access to this process)')
 
 
 # for process in psutil.process_iter():
