c881f0dd28ce6069f8603f0ae1d179bcac28f8c9
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-03-31 23:49:54 -0600

    latest updates, using Python to automate git

--- python/guiAutomation/keyboardLight/keyboardLight.py
+++ python/guiAutomation/keyboardLight/keyboardLight.py
@@ -1,3 +1,11 @@
 import pyautogui
 
-pyautogui.press('a)
+
+# print(pyautogui.KEYBOARD_KEYS)
+# pyautogui.press('help')
+# pyautogui.hotkey('fn', 'f10')
+
+pyautogui.keyDown('fn')
+pyautogui.keyDown('f10')
+pyautogui.keyUp('f10')
+pyautogui.keyUp('fn')
