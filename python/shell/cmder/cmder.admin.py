e7c59e09cbe7d0af17d6cd58b5644bc823563f0b
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-04-14 18:34:33 -0600

    latest updates, using Python to automate git

--- python/shell/cmder/cmder.admin.py
+++ python/shell/cmder/cmder.admin.py
@@ -1,18 +1,5 @@
-from pathlib import Path
-pathToThisPythonFile = Path(__file__).resolve()
-import sys
-sys.path.append(str(Path(pathToThisPythonFile.parents[2], 'myPythonLibrary')))
-import _myPyFunc
+import os
 
-sys.path.append(str(Path(_myPyFunc.replacePartOfPath(pathToThisPythonFile.parents[0], 'publicProjects', 'privateData'))))
-import phrase
+powershellCommand = "Start-Process \"cmder.exe\" -Verb RunAs"
+os.system('Powershell -Command ' + powershellCommand)
 
-import subprocess
-
-# print(phrase.phrase)
-subprocessObj = subprocess.Popen(['runas', '/noprofile', '/user:Administrator', 'NeedsAdminPrivilege.exe'], stdin=subprocess.PIPE)
-# subprocessObj.stdin.write(phrase.phrase)
-# subprocessObj.communicate()
-
-
-# subprocess.Popen('cmder')
