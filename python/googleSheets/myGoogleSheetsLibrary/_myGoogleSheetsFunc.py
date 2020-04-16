fa584e3e2dfedd1933c56ecd5b30ee4fb220f49d
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-04-15 20:04:29 -0600

    2020-04-15 20:04, latest updates, using Python to commit

--- python/googleSheets/myGoogleSheetsLibrary/_myGoogleSheetsFunc.py
+++ python/googleSheets/myGoogleSheetsLibrary/_myGoogleSheetsFunc.py
@@ -4,15 +4,6 @@ from pprint import pprint as pp
 
 
 
-def hasFormattedValue(cell):
-
-    for item in cell:
-        if "formattedValue" in item:
-            return True
-
-    return False
-
-
 
 def isWhite(cell):
 
@@ -25,6 +16,13 @@ def isWhite(cell):
     return False
 
 
+def hasFormattedValue(cell):
+
+    for item in cell:
+        if "formattedValue" in item:
+            return True
+
+    return False
 
 
 
@@ -461,3 +459,123 @@ def authFunc(*optionalParameterPathToGoogleCredentials):
 
 
     return googleapiclient.discovery.build("sheets", "v4", credentials=credentialsObj).spreadsheets()
+
+
+
+
+
+
+def getGoogleSheetsAPIObj(partialPathArray):
+
+    from pathlib import Path
+    pathToThisPythonFile = Path(__file__).resolve()
+    import sys
+    sys.path.append(str(Path(pathToThisPythonFile.parents[2], 'myPythonLibrary')))
+    import _myPyFunc
+    
+    import pickle, googleapiclient.discovery, google_auth_oauthlib.flow, google.auth.transport.requests
+    from pprint import pprint as pp
+
+
+    pathToRepos = _myPyFunc.getParentalDirectory(pathToThisPythonFile, 'repos')
+
+    pathToJSONForCredentialsRetrieval = pathToRepos
+
+    for pathPart in partialPathArray:
+        pathToJSONForCredentialsRetrieval = Path(pathToJSONForCredentialsRetrieval, pathPart)
+    
+    pathToJSONForCredentialsRetrieval = Path(pathToJSONForCredentialsRetrieval, 'jsonForCredentialsRetrieval.json')
+ 
+ 
+    pathToPickleFileWithCredentials = pathToRepos
+
+    for pathPart in partialPathArray:
+        pathToPickleFileWithCredentials = Path(pathToPickleFileWithCredentials, pathPart)
+    
+    pathToPickleFileWithCredentials = Path(pathToPickleFileWithCredentials, 'pickleFileWithCredentials.pickle')
+
+
+    googleSheetsAPIScopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    credentialsObj = None
+
+    #if the pickle is file is available from persistent memory, then get the credentials object from it
+    #otherwise, check if the credentials object can be refreshed and do that
+    #if the credentials object can't be refreshed, then get them
+
+    if Path.exists(pathToPickleFileWithCredentials):
+        
+        with open(pathToPickleFileWithCredentials, "rb") as pickleFileObj:
+            credentialsObj = pickle.load(pickleFileObj)
+    else:
+
+        if not credentialsObj or not credentialsObj.valid:
+            if credentialsObj and credentialsObj.expired and credentialsObj.refresh_token:
+                credentialsObj.refresh(google.auth.transport.requests.Request())
+            else:
+                flowObj = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(pathToJSONForCredentialsRetrieval, googleSheetsAPIScopes)
+                credentialsObj = flowObj.run_local_server(port=0)
+            
+            #save the credentials in persistent memeory
+            
+            with open(pathToPickleFileWithCredentials, "wb") as pickleFileObj:
+                pickle.dump(credentialsObj, pickleFileObj)
+
+    return googleapiclient.discovery.build("sheets", "v4", credentials=credentialsObj).spreadsheets()
+
+
+
+
+
+def getDataWithGridForRange(spreadsheetIDStr, googleSheetsAPIObj, rangesArgument):
+    return googleSheetsAPIObj.get(spreadsheetId=spreadsheetIDStr, includeGridData=True, ranges=rangesArgument).execute()
+
+
+
+
+
+def getDataInJSONFormat(spreadsheetIDStr, googleSheetsAPIObj, *optionalArguments):
+    
+    fieldMask = optionalArguments[0]
+
+    if fieldMask:
+        return googleSheetsAPIObj.get(spreadsheetId=spreadsheetIDStr, fields=fieldMask).execute()
+    else:
+        return googleSheetsAPIObj.get(spreadsheetId=spreadsheetIDStr).execute()
+
+
+
+
+def getJSONForSheet(googleSpreadsheetDataInJSONFormat, sheetName):
+
+    for sheet in googleSpreadsheetDataInJSONFormat['sheets']:
+        sheetNameFromJSON = sheet['properties']['title']
+
+        if sheetName == sheetNameFromJSON:
+
+            return sheet
+           
+
+
+def getArrayFromJSONData(googleSpreadsheetDataInJSONFormat):
+
+    arrayToReturn = []
+
+    sheetDataAsObjects = googleSpreadsheetDataInJSONFormat['data']
+
+    for sheetDataObj in sheetDataAsObjects:
+        
+        dataObjToRetrieve = 'rowData'
+            
+        if dataObjToRetrieve in sheetDataObj:
+                            
+            for rowObject in sheetDataObj[dataObjToRetrieve]:
+
+                rowArray = []
+
+                for cellDataObj in rowObject['values']:
+                    
+                    rowArray.append(cellDataObj['formattedValue'])
+
+                arrayToReturn.append(rowArray)
+
+    return arrayToReturn
