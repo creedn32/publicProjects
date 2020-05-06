64ca21df119ec2e141af6202ea50e7627ee1fb60
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-06 10:29:31 -0600

    2020-05-06 10:29, latest updates, using Python to commit

---
+++ nodejs/reverseString.js
@@ -0,0 +1,23 @@
+
+function main() {
+
+    //get input from commandline into array
+    let inputArray = process.argv.splice(2)
+
+    //get string from the input array
+    let inputString = inputArray.join(' ')
+
+    //split string into array
+    let splitArray = inputString.split('')
+
+    //reverse the array
+    let reversedArray = splitArray.reverse()
+
+    //join the reversed array
+    let joinedString = reversedArray.join('')
+
+    //print the reversed string
+    console.log(joinedString)
+}
+
+main()
