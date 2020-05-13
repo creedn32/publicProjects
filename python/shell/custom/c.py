5c8eaa63a41e841c6da5f85e66cf628eb7207fd7
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-13 01:04:23 -0600

    2020-05-13 01:04, latest updates, using Python to commit

---
+++ python/shell/custom/c.py
@@ -0,0 +1,30 @@
+#local application imports
+# from pathlib import Path
+import sys
+# pathToThisPythonFile = Path(__file__).resolve()
+# sys.path.append(str(pathToThisPythonFile.parents[1]))
+# import myPythonLibrary._myPyFunc as _myPyFunc
+
+#standard library imports
+# import datetime
+from pprint import pprint as p
+# import psutil
+# from runpy import run_path
+# import subprocess
+
+#third-party imports
+# import gspread
+
+firstArgumentStr = sys.argv[1]
+functionToCall = firstArgumentStr[:firstArgumentStr.index('.')]
+remainingStr = firstArgumentStr[firstArgumentStr.index('.') + 1:]
+remainingArguments = remainingStr.replace('.', ' ')
+
+for argument in sys.argv[2:]:
+    remainingArguments = remainingArguments + ' ' + argument
+
+
+
+
+
+
