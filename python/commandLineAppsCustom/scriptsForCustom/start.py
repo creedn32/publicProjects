19b35b10a99c4ca575d8946e3572edb6e513b136
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-14 11:54:06 -0600

    2020-05-14 11:54, latest updates, using Python to commit

--- python/commandLineAppsCustom/scriptsForCustom/start.py
+++ python/commandLineAppsCustom/scriptsForCustom/start.py
@@ -6,32 +6,35 @@ sys.path.append(str(pathToThisPythonFile.parents[2]))
 import myPythonLibrary._myPyFunc as _myPyFunc
 import googleSheets.processIsNotRunning.processIsNotRunning as processIsNotRunning
 
-# #standard library imports
-# import datetime
+#standard library imports
 from pprint import pprint as p
 from runpy import run_path
-# import psutil
-# from runpy import run_path
-# import subprocess
-
-# #third-party imports
-# import gspread
+import subprocess
 
 
+#try from commandline as well
+#delete b.py
 
 
 def mainFunction(arrayOfArguments):
-    pathToAppCollectionsToStart = Path(_myPyFunc.replacePartOfPath(pathToThisPythonFile.parents[0], 'publicProjects', 'privateData'), 'start', 'appCollectionsToStart.py')
-    importedAppCollectionsToStart = run_path(str(pathToAppCollectionsToStart))
-    appCollectionsToStartObj = importedAppCollectionsToStart.get('appCollectionsToStartObj')
-    appCollectionToStart = appCollectionsToStartObj[arrayOfArguments[0]][arrayOfArguments[1]]
 
-    for appToStart in appCollectionToStart:
-        p(processIsNotRunning.mainFunction(appToStart))
+    pathToProcessCollectionsToStart = Path(_myPyFunc.replacePartOfPath(pathToThisPythonFile.parents[0], 'publicProjects', 'privateData'), 'start', 'processCollectionsToStart.py')
+    importedProcessCollectionsToStart = run_path(str(pathToProcessCollectionsToStart))
+    processCollectionsToStartObj = importedProcessCollectionsToStart.get('processCollectionsToStartObj')
+    processCollectionToStart = processCollectionsToStartObj[arrayOfArguments[0]][arrayOfArguments[1]]
 
-    # p(pathToAppCollectionsToStart)
-    # p(arrayOfArguments)
+    arrayOfArguments[0] = '{}.{}'.format(arrayOfArguments[0], arrayOfArguments[1])
 
+    if len(arrayOfArguments) < 3:
+        arrayOfArguments.append('dontOutputToGoogleSheets')
+
+    for processToStart in processCollectionToStart:
+
+        arrayOfArguments[1] = processToStart
+
+        if processIsNotRunning.mainFunction(arrayOfArguments):
+            p('Starting the process...')
+            subprocess.Popen(processToStart)
 
 
 if __name__ == "__main__":
