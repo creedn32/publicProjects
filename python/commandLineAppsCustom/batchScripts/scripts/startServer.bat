13adfb20cdbfce9e4f13f92c30c6909e101b9d2b
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-16 13:18:53 -0600

    2020-05-16 13:18, latest updates, using Python to commit

---
+++ python/commandLineAppsCustom/batchScripts/scripts/startServer.bat
@@ -0,0 +1,2 @@
+@echo off 
+python %~dp0/../../../fundamentals/webAppFlask/startServer.py %*
--- python/fundamentals/webAppFlask/startServer.py
+++ python/fundamentals/webAppFlask/startServer.py
@@ -1,9 +1,9 @@
-from flask import Flask
+import flask
 
-flaskApp = Flask(__name__)
-@flaskApp.route('/')
+flaskApp = flask.Flask(__name__, static_url_path='')
 
-def jsonReturnFunction():
+@flaskApp.route('/cat')
+def returnCatDetails():
 
    jsonToReturn = {
         'cat eyes': 'yellow',
@@ -13,5 +13,25 @@ def jsonReturnFunction():
    return str(jsonToReturn)
 
 
+
+# @flaskApp.route('/') #<path:filename>')
+# def returnMainPage():
+
+#    return flask.send_from_directory('../frontend', filename)
+   
+
+
+@flaskApp.route('/') #<path:filename>')
+def returnMainPage():
+
+   return flask.render_template('../frontend', filename)
+
+
+
+
 if __name__ == '__main__':
+    
+    from waitress import serve
+    serve(flaskApp, host='0.0.0.0', port=8000)
+
     flaskApp.run()
