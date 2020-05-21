0d2729b148d3da8bdb87cb1268f6a35f53279db8
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-21 15:12:39 -0600

    2020-05-21 15:12, latest updates, using Python to commit

--- python/customCommandLineApps/pythonScripts/recreatebatchscripts.py
+++ python/customCommandLineApps/pythonScripts/recreatebatchscripts.py
@@ -2,8 +2,8 @@
 from pathlib import Path
 import sys
 pathToThisPythonFile = Path(__file__).resolve()
-sys.path.append(str(pathToThisPythonFile.parents[2]))
-import myPythonLibrary._myPyFunc as _myPyFunc
+sys.path.append(str(Path(pathToThisPythonFile.parents[3], 'herokuGorilla', 'backend', 'python')))
+import horseStable.clydesdale as clydesdale
 import googleSheets.processIsNotRunning.processIsNotRunning as processIsNotRunning
 
 #standard library imports
@@ -24,7 +24,7 @@ def mainFunction(arrayOfArguments):
 
     pathToThisPythonFile = Path(__file__).resolve()
 
-    pathToPublicProjectsPython = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'python')
+    pathToPublicProjectsPython = clydesdale.getPathUpFolderTree(pathToThisPythonFile, 'python')
     pathToBatchScriptsFolder = Path(pathToPublicProjectsPython, 'customCommandLineApps', 'batchScripts', 'scripts')
 
     def arrayOfSubFolders(pathToFolder):
