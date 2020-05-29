eeb02652e16e2d38cdb0abd067821a012646035b
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-05-29 10:38:08 -0600

    2020-05-29 10:38, latest updates, using Python to commit

--- python/fundamentals/encryption/encryption.py
+++ python/fundamentals/encryption/encryption.py
@@ -5,87 +5,23 @@ import sys
 sys.path.append(str(Path(pathToThisPythonFile.parents[3], 'herokuGorilla', 'backend', 'python', 'myPythonLibrary')))
 import _myPyFunc
 
-from cryptography.fernet import Fernet
 from pprint import pprint as p
 
 
 
-def generateKey(pathToStoreKeyFile):
-    """
-    Generates a key and save it into a file
-    """
-    generatedKey = Fernet.generate_key()
 
-    with open(pathToStoreKeyFile, 'wb') as keyFileObj:
-        keyFileObj.write(generatedKey)
+pathToRepos = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'repos')
+pathToKeyFile = Path(pathToRepos, 'privateData', 'python', 'googleCredentials', 'keyForEncryption.key')
+# _myPyFunc.generateKey(pathToKeyFile)
 
-
-
-def openSavedKey(pathToKeyFile):
-    """
-    Loads the key from the current directory named `keyFile.key`
-    """
-
-    return open(pathToKeyFile, 'rb').read()
-
-
-
-def encryptFile(pathOfFileToProcess, savedKey):
-    """
-    Given a pathOfFileToProcess (str) and key (bytes), it encrypts the file and write it
-    """
-
-    fernetObjUsingKey = Fernet(savedKey)
-
-    with open(pathOfFileToProcess, "rb") as fileObj:
-        # read all file data
-        fileData = fileObj.read()
-
-
-    # encrypt data
-    encryptedFileData = fernetObjUsingKey.encrypt(fileData)
-
-    # write the encrypted file
-    with open(pathOfFileToProcess, "wb") as fileObj:
-        fileObj.write(encryptedFileData)
-
-
-
-def decryptFile(pathOfFileToProcess, savedKey):
-    """
-    Given a pathOfFileToProcess (str) and key (bytes), it decrypts the file and write it
-    """
-
-    fernetObjUsingKey = Fernet(savedKey)
-
-    with open(pathOfFileToProcess, "rb") as fileObj:
-        # read the encrypted data
-        encryptedFileData = fileObj.read()
-
-    # decrypt data
-    decryptedFileData = fernetObjUsingKey.decrypt(encryptedFileData)
-
-    # write the original file
-    with open(pathOfFileToProcess, "wb") as fileObj:
-        fileObj.write(decryptedFileData)
-
-
-
-
-
-pathToKeyFileDirectory = _myPyFunc.replacePartOfPath(pathToThisPythonFile.parents[0], 'publicProjects', 'privateData')
-keyFileName = 'keyForEncryption.key'
-pathToStoreKeyFile = Path(pathToKeyFileDirectory, keyFileName)
-# generateKey(pathToStoreKeyFile)
-
-savedKey = openSavedKey(Path(pathToKeyFileDirectory, keyFileName))
+savedKey = _myPyFunc.openSavedKey(pathToKeyFile)
 # savedKey = "asdfs"
 
-pathOfFileToProcess = Path(pathToThisPythonFile.parents[0], 'fileToProcess.txt')
+pathOfFileToProcess = Path(pathToThisPythonFile.parents[3], 'herokuGorilla', 'backend', 'jsonWithAPIKey.json')
 
-# encryptFile(pathOfFileToProcess, savedKey)
+_myPyFunc.encryptFile(pathOfFileToProcess, savedKey)
 
-# decryptFile(pathOfFileToProcess, savedKey)
+# _myPyFunc.decryptFile(pathOfFileToProcess, savedKey)
 
 
 
