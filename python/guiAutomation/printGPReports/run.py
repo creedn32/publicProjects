d6c466c5c8b245abb8504750b48d1cd14ae3534d
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-08-12 16:23:31 -0600

    2020-08-12 16:23, latest updates, using Python to commit

--- python/guiAutomation/printGPReports/run.py
+++ python/guiAutomation/printGPReports/run.py
@@ -11,41 +11,60 @@ import pandas as pd
 import pynput
 
 
-daterange = pd.date_range('7/26/20', '8/9/20')
+pandasDateRange = pd.date_range('7/28/20', '8/9/20')
+p(type(pandasDateRange))
 
-for single_date in daterange:
-    print (single_date.strftime('%m/%d/%Y'))
+if _myPyFunc.numLockIsOff():
+    g.press('numlock')
 
-    with pynput.mouse.Listener(on_click=_myPyFunc.functionOnClick) as listenerObj:
-        print("Click on 'Clear' to begin...")
-        listenerObj.join()
 
-    g.press(['tab']*(4))
+for singleDate in pandasDateRange:
+    p(singleDate.strftime('%m/%d/%Y'))
+
+    gpClearCoordinates = None
+
+    while not gpClearCoordinates:
+        gpClearCoordinates = g.locateCenterOnScreen('gpClear.png', confidence=.9)
+
+    g.click(gpClearCoordinates)    
+
+    _myPyFunc.typeCharactersOnRemoteDesktop('Summary', g.PAUSE, pause=.5)
+    g.press(['tab']*4)
+    g.press(['down', 'up', 'up', 'up'])
+    g.write(singleDate.strftime('%m/%d/%Y'))
+    g.press(['tab']*20)
+    g.press(['enter'])
+
+    while not g.locateOnScreen('gpReportDestination.png'):
+        pass
     
-    # locatedBoxFrom = g.locateOnScreen('gpHATB.png')
-    # g.moveTo(locatedBoxFrom.left + 40, locatedBoxFrom.top + 10)
-    # g.click()
-
-    # g.press(['delete']*10)
-    # g.write('01/01/' + str(year))
-    # g.press('tab')
-    # g.write('12/31/' + str(year))
-    # g.press('tab')
-
-    # time.sleep(1)
-    # locatedBoxNetCashFlow = g.locateOnScreen('quickBooksNoCashFlow.png')
-
-    # if locatedBoxNetCashFlow:
-    #     p('There are no cash flows')
-    # else: 
-    #     p('There are cash flows')
-    #     g.hotkey('alt', 'x')
-    #     g.press('n')
-    #     time.sleep(1)
-    #     g.hotkey('alt', 'x')
-    #     time.sleep(5)
-    #     # g.press['alt', 'f', exce]
+    g.press('enter')
 
 
+    gpHATBScreenOutputPrintCoordinates = None
+
+    while not gpHATBScreenOutputPrintCoordinates:
+        gpHATBScreenOutputPrintCoordinates = g.locateCenterOnScreen('gpHATBScreenOutput.png')
+
+    g.click(gpHATBScreenOutputPrintCoordinates)
+
+    while not g.locateOnScreen('gpPrint.png'):
+        pass
+
+    g.press(['c', 'u', 'enter'])
+    
+    while not g.locateOnScreen('gpSaveAs.png'):
+        pass
+    
+    g.press(['tab']*5)
+
+    _myPyFunc.typeCharactersOnRemoteDesktop('Y:\\GL Reconciliations\\GP Reports - Historical Aged Trial Balance\\' + singleDate.strftime('%Y%m%d'), g.PAUSE
+    )
+
+    # g.press('enter')
+    # g.hotkey('alt', 'f4')
 
     break
+
+if not _myPyFunc.numLockIsOff():
+    g.press('numlock')
