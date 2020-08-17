f4ae000fecf2dea0c01613ac6ca7649a5b6f8c02
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-08-17 13:35:53 -0600

    2020-08-17 13:35, latest updates, using Python to commit

--- python/regularExpressions/regularExpressions.py
+++ python/regularExpressions/regularExpressions.py
@@ -7,30 +7,42 @@ import _myPyFunc
 import re, time
 from pprint import pprint as p
 
-
 startTime = time.time()
-
-
 strOfTextFilePath = str(Path(pathToThisPythonFile.parents[4], 'organizingFiles', 'google', 'gmail', 'singleTextFile', 'singleTextFile.txt'))
 
 with open(strOfTextFilePath, 'r', encoding='utf8') as fileObj:
     strToSearch = fileObj.read() #.replace('\n', '')
 
 strToSearch = ''.join(strToSearch)
-# strToSearch = strToSearch[:1000000]
-
-
-regExPattern = re.compile(r'(?i)(.{,20})(\s*books)(.{,20})')
-# regExPattern = re.compile(r'(?i)(\s*)(\S*)(\s*)(books)(\s*)(.{,10})')
-# regExPattern = re.compile(r'(?i)\b(?!Quick|audio|work|of|on|e|All|reading|Kindle|in|many|Note|Abe|quality|memory|favorite|the|five|major|my|bestselling|flagship|i|signed|help|by|more|used|cook|fitness|400|BiggerPockets|leather|print|Audible|additional|newest|new|great|collectible|from|selling|text|rehab|browse|rare|for|nook|digital|free|read|photo|paperback|level|with|our|your|s|hand|option|buying|noreplyface)\w+(?=\s?books)')
-
-
-phrasesToExclude = ['Quick', 'audio', 'work', 'of', 'on', 'e', 'All', 'reading', 'Kindle', 'in', 'many', 'Note', 'Abe', 'quality', 'memory', 'favorite', 'the', 'five', 'major', 'my', 'bestselling', 'flagship', 'i', 'signed', 'help', 'by', 'more', 'used', 'cook', 'fitness', '400', 'BiggerPockets', 'leather', 'print', 'Audible', 'additional', 'newest', 'new', 'great', 'collectible', 'from', 'selling', 'text', 'rehab', 'browse', 'rare', 'for', 'nook', 'digital', 'free', 'read', 'photo', 'paperback', 'level', 'with', 'our', 'your', 's', 'hand', 'option', 'buying', 'Advantage', 'Owls']
-phrasesToExcludeLowerCase = [x.lower() for x in phrasesToExclude]
+strToSearch = strToSearch[:4000000]
 
 
 
-for match in re.findall(regExPattern, strToSearch):
+
+def phraseNotInArrays(phraseToCheck, phrasesToExclude, partialPhrasesToExclude):
+
+    for phraseFromArray in phrasesToExclude:
+        if phraseFromArray.lower() == phraseToCheck.lower():
+            return False
+
+    for phraseFromArray in partialPhrasesToExclude:
+        if phraseFromArray.lower() in phraseToCheck.lower():
+            return False
+
+    return True
+
+
+phrasesToExclude = ['audio', 'work', 'of', 'on', 'e', 'All', 'reading', 'Kindle', 'in', 'many', 'Note', 'Abe', 'quality', 'memory', 'favorite', 'the', 'five', 'major', 'my', 'bestselling', 'flagship', 'i', 'signed', 'help', 'by', 'more', 'used', 'cook', 'fitness', '400', 'BiggerPockets', 'leather', 'print', 'Audible', 'additional', 'newest', 'new', 'great', 'collectible', 'from', 'selling', 'text', 'rehab', 'browse', 'rare', 'for', 'nook', 'digital', 'free', 'read', 'photo', 'paperback', 'level', 'with', 'our', 'your', 's', 'hand', 'option', 'buying', 'Advantage', 'Owls' \
+    'BYU', 'their', 'Dalton']
+
+partialPhrasesToExclude = ['Quick']
+
+# phrasesToExclude = []
+# partialPhrasesToExclude = []
+
+findAllResults = re.findall(regExPattern, strToSearch)
+
+for match in findAllResults:
     matchList = list(match)
 
     try:
@@ -38,10 +50,11 @@ for match in re.findall(regExPattern, strToSearch):
     except:
         matchList.append(match[0])
 
-    if matchList[3].lower() not in phrasesToExcludeLowerCase:
+    if phraseNotInArrays(matchList[3].lower(), phrasesToExclude, partialPhrasesToExclude):
         p(matchList)
 
+p(len(findAllResults))
 
-bookstores = ['Advantage Books', 'Owls Books', 'Abe Books']
+bookstores = ['Advantage Books', 'Owls Books', 'Abe Books', 'Dalton Books']
 
 _myPyFunc.printElapsedTime(startTime, 'Done')
