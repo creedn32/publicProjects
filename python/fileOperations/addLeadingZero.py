4467a3fd0419c8cdc9e55b0738caec060ba251ed
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-04 16:05:17 -0600

    2020-09-04 16:05, latest updates, using Python to commit

--- python/fileOperations/addLeadingZero.py
+++ python/fileOperations/addLeadingZero.py
@@ -11,19 +11,20 @@ import re
 
 
 for fileObj in Path(sys.argv[1]).iterdir():
-    # result = re.search('(.*-)(.*)(\..*)', fileObj.name)
+    result = re.search('(.*-)(.*)(\..*)', fileObj.name)
     
     newFilePath = fileObj
     
-    # if len(result.group(2)) == 1:
-    #     newFilePath = str(fileObj.parents[0]) + result.group(1) + '0' + result.group(2) + result.group(3)
-    #     p(str(fileObj.parents[0]) + result.group(1) + '0' + result.group(2) + result.group(3))
-    
-    if 'ytellingStorytelling' in fileObj.name:
-        strToUse = str(fileObj.parents[0]) + '\\' + str(fileObj.name[8:])
+    if len(result.group(2)) == 1:
+        strToUse = str(fileObj.parents[0]) + '//' + result.group(1) + '0' + result.group(2) + result.group(3)
+        # newFilePath = strToUse
         p(strToUse)
-        # newFilePath = str(fileObj.parents[0]) + result.group(1) + '0' + result.group(2) + result.group(3)
-        newFilePath = strToUse
+    
+    # if 'ytellingStorytelling' in fileObj.name:
+    #     strToUse = str(fileObj.parents[0]) + '\\' + str(fileObj.name[8:])
+    #     p(strToUse)
+    #     # newFilePath = str(fileObj.parents[0]) + result.group(1) + '0' + result.group(2) + result.group(3)
+    #     newFilePath = strToUse
         
     os.rename(str(fileObj), newFilePath)
     
