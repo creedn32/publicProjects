1ec7df06c4ee2a4f366df1ce4513f715a590981e
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-04 19:42:16 -0600

    2020-09-04 19:42, latest updates, using Python to commit

--- python/fileOperations/renameFiles.py
+++ python/fileOperations/renameFiles.py
@@ -12,26 +12,30 @@ import re
 
 for fileObj in Path(sys.argv[1]).iterdir():
 
-    result = re.search('(.*-)(.*)(\..*)', fileObj.name)
+    # result = re.search('(.*-)(.*)(\..*)', fileObj.name)
     
     newFilePath = fileObj
     
-    if len(result.group(2)) == 1:
-        strToUse = str(fileObj.parents[0]) + '//' + result.group(1) + result.group(2) + '0' + result.group(3)
-        newFilePath = strToUse
+    # if len(result.group(2)) == 1:
+    #     strToUse = str(fileObj.parents[0]) + '//' + result.group(1) + result.group(2) + '0' + result.group(3)
+    #     newFilePath = strToUse
         
-        p(strToUse)
+    #     p(strToUse)
 
-    if result.group(2) == '10':
-        strToUse = str(fileObj.parents[0]) + '//' + result.group(1) + '19' + result.group(3)
-        newFilePath = strToUse
+    # if result.group(2) == '10':
+    #     strToUse = str(fileObj.parents[0]) + '//' + result.group(1) + '19' + result.group(3)
+    #     newFilePath = strToUse
         
-        p(strToUse)
+    #     p(strToUse)
     
     # if 'ytellingStorytelling' in fileObj.name:
     #     strToUse = str(fileObj.parents[0]) + '\\' + str(fileObj.name[8:])
     #     p(strToUse)
     #     # newFilePath = str(fileObj.parents[0]) + result.group(1) + '0' + result.group(2) + result.group(3)
     #     newFilePath = strToUse
+
+    strToUse = str(fileObj.parents[0]) + '\\' + 'Re - Spoken Books - ' + str(fileObj.name)
+    newFilePath = strToUse
+    p(strToUse)
 
     os.rename(str(fileObj), newFilePath)
