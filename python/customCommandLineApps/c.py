f9c4750d74c79429522823561406983d5bc420ea
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-10 23:48:25 -0600

    2020-09-10 23:48, latest updates, using Python to commit

--- python/customCommandLineApps/c.py
+++ python/customCommandLineApps/c.py
@@ -1,17 +1,36 @@
-#standard library imports
-from pprint import pprint as p
+from pathlib import Path
+pathToThisPythonFile = Path(__file__).resolve()
 import sys
+pathToAppend = Path(pathToThisPythonFile.parents[2], 'herokuGorilla', 'backend', 'python', 'myPythonLibrary')
+sys.path.append(str(pathToAppend))
+import _myPyFunc
 
-
+from pprint import pprint as p
 
 def mainFunction():
 
-    # argumentsArray = sys.argv[1].split('.') + sys.argv[2:]
+    p("Searching for command '{}.py' ...".format(sys.argv[1]))
 
+    # argumentsArray = sys.argv[1].split('.') + sys.argv[2:]
     # print(sys.argv)
+    
+    pathToRepos = _myPyFunc.getPathUpFolderTree(pathToThisPythonFile, 'repos')
+    # p(pathToRepos)
+
+    def actionToPerformOnEachFileObj(currentFolder):
+
+        for node in currentFolder.iterdir():
+            if node.is_file() and node.suffix == '.py':
+                if node.stem == 'git':
+                    return node.parents[0]
+        
+        return None
+
+    pathToPythonFileForImport = _myPyFunc.operateOnAllFileObj(pathToRepos, actionToPerformOnEachFileObj, pathsToExclude=[Path(pathToRepos, '.history'), Path(pathToRepos, '.vscode'), Path(pathToRepos, 'reposFromOthers')])
+
+
     # is equivalent to: from os import path as imported
     importedModule = getattr(__import__('pythonScripts', fromlist=[sys.argv[1]]), sys.argv[1])
-
     importedModule.mainFunction(sys.argv[1:])
 
 
