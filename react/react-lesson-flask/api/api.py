9cddd3c60d42e30f843695fe25843bf2a03ab6de
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-09-30 21:50:25 -0600

    2020-09-30 21:49, latest updates, using Python to commit

--- react/react-lesson-flask/api/api.py
+++ react/react-lesson-flask/api/api.py
@@ -1,8 +1,14 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
+    print('Python is returning a object with the current time to the front end...')
     return {'time': time.time()}
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
---
+++ react/react-lesson-flask/build/precache-manifest.ae562ff6c96ce25d839ada341829e96f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3902633d8ea9741310d3227b85137e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f4df14e545d454bf63d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a21098ce79178fca0378",
+    "url": "/static/js/2.bef27c7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bef27c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4df14e545d454bf63d",
+    "url": "/static/js/main.55c8ced0.chunk.js"
+  },
+  {
+    "revision": "a99d9573887aee5dc6e7",
+    "url": "/static/js/runtime-main.0b90b9ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
