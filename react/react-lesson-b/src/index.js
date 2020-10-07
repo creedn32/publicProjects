80f8139d77ceb3a9e436db7126c45501599f5147
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-10-07 13:14:59 -0600

    2020-10-07 13:14, latest updates, using Python to commit

--- react/react-lesson-b/src/index.js
+++ react/react-lesson-b/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
