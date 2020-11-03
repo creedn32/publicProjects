7b73c5d92462d4bcf605c3a852c14d5a78f1316e
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-11-03 13:40:40 -0700

    2020-11-03 13:40, added/committed/pushed using custom Python script

--- python/fundamentals/xml/addToCalls.py
+++ python/fundamentals/xml/addToCalls.py
@@ -60,12 +60,10 @@ def csvRowInXML(currentCSVRow, root):
     return None
 
 
-def mainFunction(arrayOfArguments):
+def addToCalls(pathStrToPriorCallsFile, pathStrToCSVFile):
 
-    pathStrToPriorCallsFile = arrayOfArguments[1]
-    pathStrToNewCallsXML = str(Path(arrayOfArguments[1]).parents[0]) + '\callsWithCallLogAnalytics.xml'
-    
-    pathStrToCSVFile = arrayOfArguments[2]
+    pathStrToNewCallsXML = str(Path(pathStrToPriorCallsFile).parents[0]) + '\callsWithCallLogAnalytics.xml'
+
     eTreeObj = etree.parse(pathStrToPriorCallsFile)
     rootObj = eTreeObj.getroot()
 
@@ -92,7 +90,7 @@ def mainFunction(arrayOfArguments):
         for currentCSVRow in csvReader:
             currentCSVRow[dateColumnIndexCSV] = datetime.strptime(currentCSVRow[dateColumnIndexCSV], '%m/%d/%Y %H:%M')
             currentCSVRow[phoneNumberColumnIndexCSV] = cleanPhoneNumber(currentCSVRow[phoneNumberColumnIndexCSV])
-            
+
         csvReader.sort(key=lambda x: x[dateColumnIndexCSV], reverse=sortOrderDesc)
 
         elementsToAppend = []
@@ -109,7 +107,8 @@ def mainFunction(arrayOfArguments):
                     'number': currentCSVRow[phoneNumberColumnIndexCSV],
                     'type': callTypeWordToNumber[currentCSVRow[callTypeColumnIndexCSV]],
                     'date': myPyFunc.dateObjToUnixMillisecondsStr(currentCSVRow[dateColumnIndexCSV]),
-                    'duration': currentCSVRow[durationColumnIndexCSV]
+                    'duration': currentCSVRow[durationColumnIndexCSV],
+                    # 'notes': 'from csv'
                 }
 
                 elementToAppend = etree.Element('call')
@@ -140,6 +139,51 @@ def mainFunction(arrayOfArguments):
     # p(len(rootObj))
 
     myPyFunc.printTimeSinceImport()
+
+    
+    # def getPhoneNumber(element):
+    #     return cleanPhoneNumber(element.get('number'))
+
+    # def allCallsFromCSV(array):
+
+    #     for element in array:
+    #         if element.get('notes') != 'from csv':
+    #             # p(element.attrib)
+    #             return False
+    #     return True
+
+    # def printArray(array):
+    #     if array and not allCallsFromCSV(array):
+    #         p('Sequential calls with same phone number:')
+    #         for element in array:
+    #             p(element.attrib)
+
+
+    # arrayOfSamePhoneNumber = []
+
+    # for elementIndex, element in enumerate(rootObj):
+
+    #     if elementIndex > 0:
+
+    #         if getPhoneNumber(element) == getPhoneNumber(rootObj[elementIndex - 1]):
+
+    #             if len(arrayOfSamePhoneNumber) == 0:
+
+    #                 arrayOfSamePhoneNumber.append(rootObj[elementIndex - 1])
+
+    #             arrayOfSamePhoneNumber.append(element)
+            
+    #         else:
+    #             printArray(arrayOfSamePhoneNumber)
+    #             arrayOfSamePhoneNumber = []
+
+    # printArray(arrayOfSamePhoneNumber)
+
+    # myPyFunc.printTimeSinceImport()
+
+
+def mainFunction(arrayOfArguments):
+    addToCalls(arrayOfArguments[1], arrayOfArguments[2])
 
 
 if __name__ == '__main__':
