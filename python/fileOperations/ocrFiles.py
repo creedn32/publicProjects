5a8298a6cd5fedf0986594f325da679538bde6c9
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-11-17 17:09:31 -0700

    2020-11-17 17:09, added/committed/pushed using custom Python script

---
+++ python/fileOperations/ocrFiles.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+pathToThisPythonFile = Path(__file__).resolve()
+import sys
+sys.path.append(str(pathToThisPythonFile.parents[2]))
+from herokuGorilla.backend.python.myPythonLibrary import myPyFunc
+
+from pprint import pprint as p
+
+
+def ocrFiles(arrayOfArguments):
+
+    def print1(dataForActionObj):
+        
+        if dataForActionObj['currentFileObj'].is_file() and dataForActionObj['currentFileObj'].suffix == '.pdf':
+            p(str(dataForActionObj['currentFileObj']))
+        return dataForActionObj
+
+    myPyFunc.onAllFileObjInTreeBreadthFirst(Path(arrayOfArguments[1]), print1)
+
+
+
+def mainFunction(arrayOfArguments):
+
+    ocrFiles(arrayOfArguments)
+
+
+if __name__ == '__main__':
+    p(str(pathToThisPythonFile.name) + ' is not being imported. It is being run directly...')
+    mainFunction(sys.argv)
+else:
+    p(str(pathToThisPythonFile.name) + ' is being imported. It is not being run directly...')
