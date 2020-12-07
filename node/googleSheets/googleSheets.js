9e9366321b0c96bbc90679fc19ebb4697fb2d269
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-12-06 23:40:26 -0700

    2020-12-06 23:40, added/committed/pushed using custom Python script

---
+++ node/googleSheets/googleSheets.js
@@ -0,0 +1,13 @@
+// var patth = require('path');
+// var fs = require('fs');
+// thisFilePathArray = path.resolve(__dirname, __filename).split(path.sep);
+
+
+
+const mainFunction = ([firstName, middleName, lastName]) => { 
+
+
+    console.log(7);
+};
+
+module.exports = mainFunction;
