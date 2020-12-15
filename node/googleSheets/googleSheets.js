694e3c5ed3ff9226c15b2017fd30daf048a8d546
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-12-14 23:02:25 -0700

    2020-12-14 23:02, added/committed/pushed using Creed's Python script

--- node/googleSheets/googleSheets.js
+++ node/googleSheets/googleSheets.js
@@ -1,15 +1,15 @@
 // var path = require('path');
 // var fs = require('fs');
 // thisFilePathArray = path.resolve(__dirname, __filename).split(path.sep);
-let l = require('../node/creedLibrary/creedLibrary')
+let l = require('../..node/creedLibrary/creedLibrary')
 var c = console.log.bind(console);
 
 const mainFunction = ([googleSheetTitle, googleSheetUsername]) => { 
 
     c(googleSheetTitle);
     c(googleSheetUsername);
-
+    // l.c(1);
 
 };
 
-module.exports = mainFunction;
+module.exports = mainFunctio
