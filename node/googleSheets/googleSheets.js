647dd67d8acdb87065b9f815087e6cb20543032c
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2020-12-20 21:12:53 -0700

    2020-12-20 21:12, added/committed/pushed using Creed's Python script

--- node/googleSheets/googleSheets.js
+++ node/googleSheets/googleSheets.js
@@ -1,100 +1,100 @@
 let path = require('path');
 pathArrayThisFile = path.resolve(__dirname, __filename).split(path.sep);
 
-// let mainLibrary = require('../creedLibrary/mainLibrary');
 let googleSheetsLibrary = require('../creedLibrary/googleSheetsLibrary/googleSheetsLibrary');
 let c = console.log.bind(console);
 const {google} = require('googleapis');
 
 const mainFunction = async ([googleSheetsUsername, googleSpreadsheetTitle, googleSheetTitle]) => {
 
-    const googleAccountLevelObj = await googleSheetsLibrary.getGoogleAccountLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle);
-    const googleSheetsValuesLevelObj = googleSheetsLibrary.getGoogleSheetsValuesLevelObj(googleAccountLevelObj);
-    const spreadsheetLevelObj = await googleSheetsLibrary.getSpreadsheetLevelObj(googleAccountLevelObj, googleSpreadsheetTitle);
-    const sheetLevelObj = googleSheetsLibrary.getSheetLevelObj(googleSheetsValuesLevelObj);
+    let sheetLevelObj = {
+
+        googleAccountLevelObj: await googleSheetsLibrary.getGoogleAccountLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle),
+        googleSheetsValuesLevelObj: googleSheetsLibrary.getGoogleSheetsValuesLevelObj(this.googleAccountLevelObj),
+        // spreadsheetLevelObj: await googleSheetsLibrary.getSpreadsheetLevelObj(this.googleAccountLevelObj, googleSpreadsheetTitle),
+        spreadsheetID: 1,
+        // spreadsheetID: googleSheetsLibrary.getGoogleSpreadsheetID(this.googleAccountLevelObj, googleSpreadsheetTitle),
+        getArrayOfValues: function() {
+
+            try {
+
+                const request = {
+
+                        spreadsheetId: this.spreadsheetID,
+                        range: googleSheetTitle,
+
+                };
+
+                // const googleSheetValues = await googleSheetsLevelObj.spreadsheets.values.get();
+
+                // let rows = googleSheetValues.data.values;
+
+                let rows = [['a', 'b'], ['c']];
+
+                if (rows.length) {
+
+                    const maxRowLength = Math.max(...rows.map(row => row.length));
+                    const rowsWithFill = rows.map(row => row.concat(Array(maxRowLength - row.length).fill('')));
+                    return rowsWithFill;
+
+                }
+
+            } catch (error) {
+
+                c('The API returned an error: ' + error);
+
+            }
+
+        },
+        updateSheet: function(arrayToUpload) {
+
+            const request = {
+
+                spreadsheetId: this.spreadsheetID,
+                // spreadsheetId: await googleSheetsLibrary.getGoogleSpreadsheetID(googleAccountLevelObj, googleSpreadsheetTitle),
+
+                resource: {
+
+                    valueInputOption: 'RAW',
+
+                    data: {
+
+                        range: googleSheetTitle,
+                        values: arrayToUpload
+                    },
+
+                },
+
+                auth: this.googleAccountLevelObj,
+
+            };
+
+            try {
+
+                // const response = await googleSheetsLevelObj.spreadsheets.values.batchUpdate(request);
+
+                response = null;
+                c(JSON.stringify(response, null, 2));
+
+            } catch (err) {
+
+                c(err);
+
+            }
+
+        }
+
+    };
+
     c(sheetLevelObj.getArrayOfValues());
 
-
-
-
-
-    // // class GoogleSheetsSheetLevelObj {
-        
-    // //     constructor() {
-
-    // //         this.googleSheetsLevelObj = 
-
-    // //     }
-
-
-    
-    // let sheetLevelObj = new GoogleSheetsSheetLevelObj();
-
-    // c(sheetLevelObj.propertyInfo);
-    // c(sheetLevelObj.getArrayOfValues());
-
-
-    // module.exports.getArrayOfValues = async (googleAccountLevelObj, googleSpreadsheetTitle, googleSheetTitle) => {
-
-    //     const googleSpreadsheetID = await module.exports.getGoogleSpreadsheetID(googleAccountLevelObj, googleSpreadsheetTitle);
-    //     const googleSheetsLevelObj = module.exports.getGoogleSheetsLevelObj(googleAccountLevelObj);
-
-    
-    // };
-
-
-
-
-
-    // c(await googleSheetsLibrary.getArrayOfValues(googleAccountLevelObj, googleSpreadsheetTitle, googleSheetTitle));
-
-
-
-
-
-
-    // const updateSheet = async () => {
-
-    //     const googleSheetsLevelObj = google.sheets({version: 'v4', auth: googleAccountLevelObj});
-
-    //     const request = {
-
-    //         spreadsheetId: await googleSheetsLibrary.getGoogleSpreadsheetID(googleAccountLevelObj, googleSpreadsheetTitle),
-
-    //         resource: {
-
-    //             valueInputOption: 'RAW',
-
-    //             data: {
-
-    //                 range: googleSheetTitle,
-    //                 values: [
-    //                     ['test1'],
-    //                     ['data1'],
-    //                     ['data1']
-    //                 ]
-    //             },
-
-    //         },
-
-    //         auth: googleAccountLevelObj,
-    //     };
-
-    //     try {
-
-    //         const response = await googleSheetsLevelObj.spreadsheets.values.batchUpdate(request);
-    //         // c(JSON.stringify(response, null, 2));
-
-    //     } catch (err) {
-
-    //         c(err);
-
-    //     }
-
-    // }
-
-    // await updateSheet();
-    
+    await sheetLevelObj.updateSheet(
+        [
+            ['test1'],
+            ['data1'],
+            ['data1']
+        ]
+    );
 }
 
 
