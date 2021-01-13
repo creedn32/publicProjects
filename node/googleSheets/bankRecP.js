30ae8e563163732ee745147dfbed01685ab8aeb4
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2021-01-13 14:39:02 -0700

    2021-01-13 14:39, added, committed, & pushed using python script

--- node/googleSheets/bankRecP.js
+++ node/googleSheets/bankRecP.js
@@ -4,10 +4,16 @@ pathArrayThisFile = path.resolve(__dirname, __filename).split(path.sep);
 let googleSheetsLibrary = require('../creedLibrary/googleSheetsLibrary/googleSheetsLibrary');
 let c = console.log.bind(console);
 
-const getSheetAndUpdateSheet = async ([googleSheetsUsername, googleSpreadsheetTitle, bankSheetTitle, bankNewSheetTitle, transfersToPostSheetTitle, transactionsToPostSheetTitle, bankName, userName, firstCheckbook, secondCheckbook, glForFees]) => {
+const getSheetAndUpdateSheet = async ([googleSheetsUsername, googleSpreadsheetTitle, receivingBankFromPayPalData, accountantName, defaultTransferFromCheckbook, defaultTransferToCheckbook]) => {
+
+    bankSheetTitle = 'bank';
+    expandedBankSheetTitle = 'expandedBank';
+    transfersToPostSheetTitle = 'transfersToPost';
+    transactionsToPostSheetTitle = 'transactionsToPost';
+    glForFees = '01-000-5321';
 
     bankSheetLevelObj = await googleSheetsLibrary.getSheetLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle, bankSheetTitle);
-    bankNewSheetLevelObj = await googleSheetsLibrary.getSheetLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle, bankNewSheetTitle);
+    bankNewSheetLevelObj = await googleSheetsLibrary.getSheetLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle, expandedBankSheetTitle);
     transfersToPostSheetLevelObj = await googleSheetsLibrary.getSheetLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle, transfersToPostSheetTitle);
     transactionsToPostSheetLevelObj = await googleSheetsLibrary.getSheetLevelObj(pathArrayThisFile, googleSheetsUsername, googleSpreadsheetTitle, transactionsToPostSheetTitle);
 
@@ -59,25 +65,29 @@ const getSheetAndUpdateSheet = async ([googleSheetsUsername, googleSpreadsheetTi
 
     expandedArrayOfValues.forEach((row) => {
 
-        if (row[10] === bankName) {
+        if (row[10] === receivingBankFromPayPalData) {
 
-            [transferFromCheckbook, transferToCheckbook] = [firstCheckbook, secondCheckbook];
+            [transferFromCheckbook, transferToCheckbook] = [defaultTransferFromCheckbook, defaultTransferToCheckbook];
 
             if (row[6] > 0) {
 
-                [transferFromCheckbook, transferToCheckbook] = [secondCheckbook, firstCheckbook];
+                [transferFromCheckbook, transferToCheckbook] = [defaultTransferToCheckbook, defaultTransferFromCheckbook];
             
             }
 
             description = transferFromCheckbook + ' to ' + transferToCheckbook;
 
-            transfersToPostArray.push([row[0], description, transferFromCheckbook, Math.abs(row[6]), transferToCheckbook, '', userName])
+            transfersToPostArray.push([row[0], description, transferFromCheckbook, Math.abs(row[6]), transferToCheckbook, '', accountantName])
 
         }
         
         if (row[5] === 'Fee') {
 
-            feesToPostArray.push(['Enter Transaction', 'Decrease Adjustment', row[0], firstCheckbook, firstCheckbook, 'Fee', Math.abs(row[6]), glForFees, Math.abs(row[6]), '', userName]);
+            adjustmentType = 'Decrease Adjustment';
+
+            if (row[6] > 0) adjustmentType = 'Increase Adjustment';
+
+            feesToPostArray.push(['Enter Transaction', adjustmentType, row[0], defaultTransferFromCheckbook, defaultTransferFromCheckbook, 'Fee', Math.abs(row[6]), glForFees, Math.abs(row[6]), '', accountantName]);
 
         }
 
