ddc94824ff868fbe5f1ffa689c21d997b72bd907
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2021-01-20 16:36:09 -0700

    2021-01-20 16:36, added, committed, & pushed using python script

--- node/browserLessGoogle/browserlessGoogle.js
+++ node/browserLessGoogle/browserlessGoogle.js
@@ -1,7 +1,8 @@
-const puppeteer = require('puppeteer');
 const c = console.log.bind(console);
+const puppeteer = require('puppeteer');
+const loginData = require('../../../privateData/node/browserlessGoogle/loginData');
 
-const browserlessGoogle = async ([username, password]) => {
+const browserlessGoogle = async () => {
 
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
@@ -15,18 +16,18 @@ const browserlessGoogle = async ([username, password]) => {
 
   emailInputSelector = 'input[type="email"]';
   await page.waitForSelector(emailInputSelector);
-  await page.type(emailInputSelector, username);
+  await page.type(emailInputSelector, loginData['username']);
 
   await page.click("#identifierNext");
   await navigationPromise;
 
   // await page.waitForSelector('input[type="password"]', { visible: true });
-  // await page.type('input[type="password"]', password);
+  // await page.type('input[type="password"]', loginData['username']);
 
   // await page.waitForSelector("#passwordNext", { visible: true });
   // await page.click("#passwordNext");
 
-  // await browser.close();
+  await browser.close();
   // await page.screenshot({path: 'screenshot.png'});
 };
 
