4bce419e36880848c02355f1082aefa1618770e1
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2021-02-19 11:40:38 -0700

    2021-02-19 11:40, added, committed, & pushed using python script

--- node/googleSheets/compareSystems.js
+++ node/googleSheets/compareSystems.js
@@ -7,6 +7,20 @@ const googleSheetsLibrary = require('../creedLibrary/googleSheetsLibrary/googleS
 
 const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetTitle, firstAccount, firstAccountReport, secondAccount, secondAccountReport, thirdAccount, thirdAccountReport, accountingSystemStandardReport]) => {
 
+
+    // var packageName = "a.b.c.d";
+    // var splitted = packageName.split('.');
+    // var json = {};
+    // var current = json;
+    // for (var i = 0; i < splitted.length; i++) {
+    //     current[splitted[i]] = {};
+    //     c(JSON.stringify(current));
+    //     current = current[splitted[i]];
+    //     c(JSON.stringify(current));
+    // }
+    
+
+
     const dataSheetTitle = 'Data';
     const comparisonSheetTitle = 'Comparison';
 
@@ -15,19 +29,15 @@ const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetT
 
     const dataArray = await dataSheetLevelObj.getArrayOfValues();
     const comparisonObj = {};
+    // let currentComparisonObj = comparisonObj;
     const emptyDataPointObj = {
 
         [firstAccount]: {
 
             [accountingSystemStandardReport]: {
 
-                'Amount': 0,
-                'Prior EOY': 0,
                 'Adjusted Amount': 0,
                 'Date Of Data Pull': '',
-                'Person': '',
-                'File Name': '',
-                'Notes': '',
 
             },
 
@@ -78,36 +88,57 @@ const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetT
     dataArray.forEach((row) => {
 
         const dateFromRow = row[0];
-        const adjustedAmountFromRow = row[7];
+        const adjustedAmountFromRow = parseFloat(row[7].replace(/,/g, '').replace(/^[(]([\d.]+).*$/gm, '-$1'));
         const dateOfDataPullFromRow = row[1];
         const accountFromRow = row[2];
         const reportFromRow = row[4];
 
-        if (dateFromRow != 'Date') {
+        if (!['Date', ''].includes(dateFromRow) && !comparisonObj.hasOwnProperty(dateFromRow)) { 
 
             comparisonObj[dateFromRow] = JSON.parse(JSON.stringify(emptyDataPointObj));
 
         }
 
-        for (const account of [firstAccount]) {
-            
-            for (const report of [accountingSystemStandardReport, firstAccountReport]) {
+        accountsAndReportsObj = {
+
+            [firstAccount]: [accountingSystemStandardReport, firstAccountReport],
+            [secondAccount]: [secondAccountReport, accountingSystemStandardReport],
+            [thirdAccount]: [thirdAccountReport, accountingSystemStandardReport],
+
+        }
+
+        // c(row);
+
+        for (const account in accountsAndReportsObj) {
+
+            for (const report of accountsAndReportsObj[account]) {
 
                 if (accountFromRow === account && reportFromRow === report) {
-    
+
+                    // propertiesToAdd = [dateFromRow, account, report];
+
+                    // for (let i = 0; i < propertiesToAdd.length; i++) {
+
+                    //     // c(i);
+                    //     currentComparisonObj[propertiesToAdd[i]] = {};
+                    //     currentComparisonObj = currentComparisonObj[propertiesToAdd[i]];
+
+                    // }
+
                     comparisonObj[dateFromRow][account][report]['Adjusted Amount'] = adjustedAmountFromRow;
                     comparisonObj[dateFromRow][account][report]['Date Of Data Pull'] = dateOfDataPullFromRow;
-        
+                    
+
                 }
 
             }
 
         }
-        
+
 
     });
 
-    // c(JSON.stringify(comparisonObj));
+
 
     comparisonArray = [
 
@@ -118,14 +149,17 @@ const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetT
             '',
             '',
             '',
+            '',
             secondAccount,
+            '',
             '',
             '',
             '',
             thirdAccount,
             '',
             '',
-            ''
+            '',
+            '',
 
         ],
         [
@@ -135,14 +169,17 @@ const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetT
             accountingSystemStandardReport + ' Date Of Data Pull',
             firstAccountReport + ' Adjusted Amount',
             firstAccountReport + ' Date Of Data Pull',
+            'Difference',
             secondAccountReport + ' Adjusted Amount',
             secondAccountReport + ' Date Of Data Pull',
             accountingSystemStandardReport + ' Adjusted Amount',
             accountingSystemStandardReport + ' Date Of Data Pull',
+            'Difference',
             thirdAccountReport + ' Adjusted Amount',
             thirdAccountReport + ' Date Of Data Pull',
             accountingSystemStandardReport + ' Adjusted Amount',
             accountingSystemStandardReport + ' Date Of Data Pull',
+            'Difference',
 
         ]
 
@@ -160,16 +197,19 @@ const getSheetAndUpdateSheet = async ([googleAccountUsername, googleSpreadsheetT
                 comparisonObj[balanceDate][firstAccount][accountingSystemStandardReport]['Date Of Data Pull'],
                 comparisonObj[balanceDate][firstAccount][firstAccountReport]['Adjusted Amount'],
                 comparisonObj[balanceDate][firstAccount][firstAccountReport]['Date Of Data Pull'],
+                '',
 
                 comparisonObj[balanceDate][secondAccount][secondAccountReport]['Adjusted Amount'],
                 comparisonObj[balanceDate][secondAccount][secondAccountReport]['Date Of Data Pull'],
                 comparisonObj[balanceDate][secondAccount][accountingSystemStandardReport]['Adjusted Amount'],
                 comparisonObj[balanceDate][secondAccount][accountingSystemStandardReport]['Date Of Data Pull'],
+                '',
 
                 comparisonObj[balanceDate][thirdAccount][thirdAccountReport]['Adjusted Amount'],
                 comparisonObj[balanceDate][thirdAccount][thirdAccountReport]['Date Of Data Pull'],
                 comparisonObj[balanceDate][thirdAccount][accountingSystemStandardReport]['Adjusted Amount'],
-                comparisonObj[balanceDate][thirdAccount][accountingSystemStandardReport]['Date Of Data Pull']
+                comparisonObj[balanceDate][thirdAccount][accountingSystemStandardReport]['Date Of Data Pull'],
+                '',
 
             ]
 
