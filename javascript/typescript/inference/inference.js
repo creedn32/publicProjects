386242b481e5534d9dc9593ee94c56d92d280f93
Creed Naylor <16467643+creednaylor@users.noreply.github.com>
2021-03-18 14:31:10 -0600

    more typescript

---
+++ javascript/typescript/inference/inference.js
@@ -0,0 +1,11 @@
+var helloWorld = 'Hello World';
+var user = {
+    name: 'Hayes',
+    id: 0
+};
+// const user2: User = {
+//   username: 'Hayes',
+//   id: 0,
+// };
+console.log(user);
+// console.log(user2);
---
+++ javascript/typescript/inference/inference.ts
@@ -0,0 +1,20 @@
+let helloWorld = 'Hello World';
+
+interface User {
+  name: string;
+  id: number;
+}
+
+const user: User = {
+  name: 'Hayes',
+  id: 0,
+};
+
+// const user2: User = {
+//   username: 'Hayes',
+//   id: 0,
+// };
+
+console.log(user);
+// console.log(user2);
+
